d092555eb9d3983dcbbcb387558a4639f5239595
Sumit Chaudhari <sumit.nitsurat@gmail.com>
2019-10-03 16:40:51 +0800

    document decryption using the key added

--- src/services/qrProcessor/index.js
+++ src/services/qrProcessor/index.js
@@ -1,19 +1,34 @@
 import { get } from "axios";
+import { decryptString } from "@govtechsg/opencerts-encryption";
 
 export const decodeQrCode = qrCode => {
   const ttRegex = /tradetrust:\/\/(.*)/;
   if (!ttRegex.test(qrCode))
     throw new Error("QR Code is not formatted to TradeTrust specifications");
   const [, encodedPayload] = ttRegex.exec(qrCode);
-  const decodedPayload = JSON.parse(decodeURIComponent(encodedPayload));
+  const decodedPayload = decodeURIComponent(encodedPayload);
   return decodedPayload;
 };
 
 export const encodeQrCode = payload =>
   `tradetrust://${encodeURIComponent(JSON.stringify(payload))}`;
 
+const deceryptDocument = async uri => {
+  const uriPart = uri.split("#");
+  const { data } = await get(uriPart[0]);
+  return JSON.parse(
+    decryptString({
+      tag: data.document.tag,
+      cipherText: data.document.cipherText,
+      iv: data.document.iv,
+      key: uriPart[1],
+      type: "OPEN-ATTESTATION-TYPE-1"
+    })
+  );
+};
+
 export const processQrCode = async qrCode => {
-  const { uri } = decodeQrCode(qrCode);
-  const { data } = await get(uri);
+  const uri = decodeQrCode(qrCode);
+  const data = await deceryptDocument(uri);
   return data;
 };
