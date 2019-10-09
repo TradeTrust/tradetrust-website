1364eefed8554a743239504263d283ceb5988611
Nebulis <nebounet@gmail.com>
2019-10-09 15:50:32 +0800

    fix: use PrintWatermark named import

--- src/pages/viewer.js
+++ src/pages/viewer.js
@@ -2,7 +2,7 @@ import React from "react";
 import NavigationBar from "../components/Layout/NavigationBar";
 import Footer from "../components/Layout/Footer";
 import ViewerPageContainer from "../components/ViewerPageContainer";
-import PrintWatermark from "../components/PrintWatermark";
+import { PrintWatermark } from "../components/PrintWatermark";
 import { isWatermarkFeatureActive } from "../config/feature-config";
 
 export const ViewerPage = () => (
