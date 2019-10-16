df222d4a1fca7848f65a203b0c0d64e6aea1e6a3
Nebulis <nebounet@gmail.com>
2019-10-16 15:17:16 +0800

    feat: add react-helmet

--- src/pages/viewer.js
+++ src/pages/viewer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import NavigationBar from "../components/Layout/NavigationBar";
 import Footer from "../components/Layout/Footer";
 import ViewerPageContainer from "../components/ViewerPageContainer";
@@ -7,6 +8,16 @@ import { isWatermarkFeatureActive } from "../config/feature-config";
 
 export const ViewerPage = () => (
   <>
+    <Helmet>
+      <meta property="og:url" content="https://tradetrust.io" />
+      <meta
+        property="og:title"
+        content="TradeTrust - An easy way to check and verify your documents"
+      />
+      <meta property="og:description" content="Add Tradetrust description" />
+      <meta property="og:url" content={`${window.location.origin}`} />
+      <title>TradeTrust - An easy way to check and verify your documents</title>
+    </Helmet>
     {isWatermarkFeatureActive && <PrintWatermark />}
     <NavigationBar />
     <ViewerPageContainer />
