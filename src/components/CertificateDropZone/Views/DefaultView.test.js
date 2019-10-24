ab8065db3eed71e64ce305dddbd4616f5b52c2fa
Sumit Chaudhari <sumit.nitsurat@gmail.com>
2019-10-24 14:15:53 +0800

    invalid styling view fixed

--- src/components/CertificateDropZone/Views/DefaultView.test.js
+++ src/components/CertificateDropZone/Views/DefaultView.test.js
@@ -4,22 +4,20 @@ import DefaultView from "./DefaultView";
 
 describe("defaultView", () => {
   it("displays correctly if accept is true", () => {
-    const wrapper = shallow(<DefaultView fileDropped={true} accept={true} toggleQrReaderVisible={() => {}} />);
+    const wrapper = shallow(<DefaultView hover={true} accept={true} toggleQrReaderVisible={() => {}} />);
     const viewerContainerElm = wrapper.find("[data-id='viewer-container']");
     expect(viewerContainerElm.text()).not.toContain("File cannot be read");
   });
 
   it("displays correctly class if accept is false", () => {
-    const wrapper = shallow(<DefaultView fileDropped={true} accept={false} toggleQrReaderVisible={() => {}} />);
+    const wrapper = shallow(<DefaultView hover={true} accept={false} toggleQrReaderVisible={() => {}} />);
     const viewerContainerElm = wrapper.find("[data-id='viewer-container']");
     expect(viewerContainerElm.text()).toContain("File cannot be read");
   });
 
   it("runs toggleQrReaderVisible when `Scan QR Code` is pressed", () => {
     const toggleQrReaderVisible = jest.fn();
-    const wrapper = shallow(
-      <DefaultView fileDropped={false} accept={true} toggleQrReaderVisible={toggleQrReaderVisible} />
-    );
+    const wrapper = shallow(<DefaultView hover={false} accept={true} toggleQrReaderVisible={toggleQrReaderVisible} />);
     const buttonElm = wrapper.find("[data-id='scan-qr-button']");
     buttonElm.simulate("click", {
       preventDefault: () => {},
