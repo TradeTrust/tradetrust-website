a1132cfc77acbd2fef3b01efda00d0bdaa989ddb
Sumit Chaudhari <sumit.nitsurat@gmail.com>
2020-01-08 13:59:03 +0800

    test added for fetching owner error

--- src/components/CertificateVerifyBlock/DetailedCertificateVerifyBlock/DetailedCertificateVerifyBlock.test.js
+++ src/components/CertificateVerifyBlock/DetailedCertificateVerifyBlock/DetailedCertificateVerifyBlock.test.js
@@ -1,17 +1,25 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 import { act } from "react-dom/test-utils";
 import DetailedCertificateVerifyBlock from ".";
 import ROPSTEN from "../../HomePageContent/Ropsten-Demo.json";
 import TokenRegistry from "../../../test/fixture/tokenRegistry.json";
+import { getTokenOwner } from "../../../services/token";
+
+jest.mock("../../../services/token", () => ({
+  ...jest.requireActual("../../../services/token"),
+  getTokenOwner: jest.fn(),
+  initializeToken: jest.fn()
+}));
 
 describe("detailedCertificateVerifyBlock", () => {
-  it("render document is token", async () => {
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+  it("should render when document is token", async () => {
     let wrapper;
     await act(async () => {
-      wrapper = shallow(
-        <DetailedCertificateVerifyBlock verificationStatus={{ valid: true }} document={TokenRegistry} />
-      );
+      wrapper = mount(<DetailedCertificateVerifyBlock verificationStatus={{ valid: true }} document={TokenRegistry} />);
     });
     wrapper.setProps();
     expect(wrapper.find("h5").text()).toStrictEqual("Details");
@@ -22,10 +30,27 @@ describe("detailedCertificateVerifyBlock", () => {
   it("doesnt render TokenVerifyBlock if document is not token", async () => {
     let wrapper;
     await act(async () => {
-      wrapper = shallow(<DetailedCertificateVerifyBlock verificationStatus={{ valid: true }} document={ROPSTEN} />);
+      wrapper = mount(<DetailedCertificateVerifyBlock verificationStatus={{ valid: true }} document={ROPSTEN} />);
     });
     wrapper.setProps();
     expect(wrapper.find("TokenVerifyBlock")).toHaveLength(0);
     expect(wrapper.find("CertificateVerifyCheck")).toHaveLength(1);
+  });
+
+  it("should throw  error if can not fetch owner", async () => {
+    getTokenOwner.mockImplementation(() => {
+      throw new Error("owner error");
+    });
+    let wrapper;
+    await act(async () => {
+      wrapper = mount(<DetailedCertificateVerifyBlock verificationStatus={{ valid: true }} document={TokenRegistry} />);
+    });
+    wrapper.setProps();
+    expect(
+      wrapper
+        .find(".text-danger")
+        .at(0)
+        .text()
+    ).toStrictEqual("owner error");
   });
 });
