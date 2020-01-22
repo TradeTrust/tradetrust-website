97f6ee2b1439c6acabd7c5ace284ae4b21aa90b1
Sumit Chaudhari <sumit.nitsurat@gmail.com>
2020-01-22 15:54:55 +0800

    test updated

--- src/components/AssetInfo/TokenSideBarField.test.tsx
+++ src/components/AssetInfo/TokenSideBarField.test.tsx
@@ -4,12 +4,14 @@ import TokenSideBarField from "./TokenSideBarField";
 
 describe("tokenSideBarField", () => {
   it("should show the correct title", () => {
-    const wrapper = mount(<TokenSideBarField title="Transfer Ownership" label="" />);
+    const wrapper = mount(
+      <TokenSideBarField id="test-transfer" title="Transfer Ownership" label="" handleClick={() => {}} />
+    );
     expect(wrapper.find("h4").text()).toStrictEqual("Transfer Ownership");
   });
 
   it("should show the correct button text, should contain 1 button", () => {
-    const wrapper = mount(<TokenSideBarField title="" label="Transfer" />);
+    const wrapper = mount(<TokenSideBarField id="test-button" title="" label="Transfer" handleClick={() => {}} />);
     expect(wrapper.find("button").text()).toStrictEqual("Transfer");
     expect(wrapper.find("button")).toHaveLength(1);
   });
