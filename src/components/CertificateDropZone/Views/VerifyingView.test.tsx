f44bad91d572bf37dc0d9a93bf81df885072e8ba
Sim Boon Long <simboonlong@gmail.com>
2020-02-14 12:41:50 +0800

    fix: dropzone overflow text (#69)

---
+++ src/components/CertificateDropZone/Views/VerifyingView.test.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { mount } from "enzyme";
+import VerifyingView from "./VerifyingView";
+
+describe("defaultView", () => {
+  it("should display correct text while verifying document", () => {
+    const wrapper = mount(<VerifyingView />);
+    expect(wrapper.find(".m-3").text()).toStrictEqual("Verifying Document...");
+  });
+});
