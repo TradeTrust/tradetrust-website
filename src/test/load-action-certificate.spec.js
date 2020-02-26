143b71278f235ed494664f876e768d5f333b4b8c
benjaminlim00 <benjaminlim1996@hotmail.sg>
2020-02-26 11:48:56 +0800

    feat: add universal actions

--- src/test/load-action-certificate.spec.js
+++ src/test/load-action-certificate.spec.js
@@ -1,56 +1,45 @@
-// import { Selector } from "testcafe";
+import { Selector } from "testcafe";
 
-// fixture("Load action from plain certificate").page`http://localhost:3000`;
+fixture("Load action from plain certificate").page`http://localhost:3000`;
 
-// const IframeBlock = Selector("#iframe");
-// const SampleTemplate = Selector("#rendered-certificate");
-// const StatusButton = Selector("#certificate-status");
-// const CertificateStatusBanner = Selector("#status-banner-container");
-// const CertificateDropzone = Selector("#certificate-dropzone");
+const IframeBlock = Selector("#iframe");
+const SampleTemplate = Selector("#root");
+const StatusButton = Selector("#certificate-status");
 
-// const validateTextContent = async (t, component, texts) =>
-//   texts.reduce(async (_prev, curr) => t.expect(component.textContent).contains(curr), Promise.resolve());
+const validateTextContent = async (t, component, texts) =>
+  texts.reduce(async (_prev, curr) => t.expect(component.textContent).contains(curr), Promise.resolve());
 
-// test("Load document from action should work when url is valid", async t => {
-//   const action = {
-//     type: "DOCUMENT",
-//     payload: {
-//       uri: `https://gist.githubusercontent.com/Nebulis/dc32c107fc5112ecf863b1dfa25995a9/raw/9aed3bfbbddaaa23d453cfbb9ee42d9efffaf2b8/opencerts-ropsten-demo.json`,
-//       redirect: "https://opencerts.io/"
-//     }
-//   };
+test("Load document from action should work when url is valid", async t => {
+  const action = {
+    type: "DOCUMENT",
+    payload: {
+      uri: `https://api.myjson.com/bins/kv1de`,
+      permittedActions: ["VIEW", "STORE"],
+      redirect: "https://dev.tradetrust.io/"
+    }
+  };
 
-//   await t.navigateTo(`http://localhost:3000/?q=${encodeURI(JSON.stringify(action))}`);
-//   await validateTextContent(t, StatusButton, [
-//     "Certificate issued by ROPSTEN: GOVERNMENT TECHNOLOGY AGENCY OF SINGAPORE (GOVTECH)"
-//   ]);
+  await t.navigateTo(`http://localhost:3000/?q=${encodeURI(JSON.stringify(action))}`);
 
-//   await validateTextContent(t, CertificateStatusBanner, [
-//     "Certificate issuer is in the SkillsFuture Singapore registry for Opencerts"
-//   ]);
+  await StatusButton.with({ visibilityCheck: true })();
 
-//   await t.switchToIframe(IframeBlock);
+  await validateTextContent(t, StatusButton, ["Issued by TRADETRUST.IO"]);
 
-//   await validateTextContent(t, SampleTemplate, [
-//     "OpenCerts Demo",
-//     "Your Name",
-//     "has successfully completed the",
-//     "John Demo"
-//   ]);
-// });
-// test("Load document from action should fail when url is invalid", async t => {
-//   const action = {
-//     type: "DOCUMENT",
-//     payload: {
-//       uri: `https://gist.githubusercontent.com/Nebulis/dc32c107fc5112ecf863b1dfa25995a9/raw/9aed3bfbbdd2b8/opencerts-ropsten-demo.json`,
-//       redirect: "https://opencerts.io/"
-//     }
-//   };
+  await t.switchToIframe(IframeBlock);
 
-//   await t.navigateTo(`http://localhost:3000/?q=${encodeURI(JSON.stringify(action))}`);
-//   await validateTextContent(t, CertificateDropzone, [
-//     "The certificate can't be loaded",
-//     "Unable to load certificate with the provided parameters",
-//     "Unable to load the certificate from https://gist.githubusercontent.com/Nebulis/dc32c107fc5112ecf863b1dfa25995a9/raw/9aed3bfbbdd2b8/opencerts-ropsten-demo.json"
-//   ]);
-// });
+  await validateTextContent(t, SampleTemplate, ["BILL OF LADING FOR OCEAN TRANSPORT OR MULTIMODAL TRANSPORT"]);
+});
+
+test("Load document from action should fail when url is invalid", async t => {
+  const action = {
+    type: "DOCUMENT",
+    payload: {
+      uri: `https://api.myjson.com/bins/error`,
+      redirect: "https://dev.tradetrust.io/"
+    }
+  };
+
+  await t.navigateTo(`http://localhost:3000/?q=${encodeURI(JSON.stringify(action))}`);
+
+  await StatusButton.with({ visibilityCheck: false })();
+});
