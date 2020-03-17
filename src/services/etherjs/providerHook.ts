e54c2ee6a4cb9f9aa9cc1194c1ffc064c6e4d0f6
Sumit Chaudhari <sumit.nitsurat@gmail.com>
2020-03-17 15:31:34 +0800

    web3 provider hook added

---
+++ src/services/etherjs/providerHook.ts
@@ -0,0 +1,47 @@
+import { useCallback, useMemo } from "react";
+import { useEthereumTransactionState, TransactionStateStatus } from "../token/helpers";
+import { ethers, providers } from "ethers";
+
+import { getLogger } from "../../utils/logger";
+const { trace, error } = getLogger("hook:etherjs:provider");
+
+interface Ethereum extends providers.Web3Provider {
+    enable: () => void;
+}
+
+declare global {
+    interface Window {
+        ethereum: Ethereum;
+        web3: {
+            currentProvider: providers.Web3Provider;
+        };
+    }
+}
+
+type GetProvider = { web3Provider: providers.Web3Provider , signer: ethers.Signer };
+
+export const useProvider = () => {
+    const [state, dispatch] = useEthereumTransactionState();
+    const setDispatchCallback = useCallback(dispatch, []);
+
+    const getProvider = () => {
+        try {
+            setDispatchCallback({ type: TransactionStateStatus.LOADING, error: undefined });
+            const { ethereum, web3 } = window;
+            const alreadyInjected = typeof ethereum !== "undefined" || typeof web3 !== "undefined";
+
+            if (!alreadyInjected) throw new Error("Metamask cannot be found");
+            const web3Provider = new ethers.providers.Web3Provider(ethereum || web3.currentProvider);
+            const signer = web3Provider.getSigner();
+            trace(`provider is ${web3Provider} and signer is ${signer}`);
+            setDispatchCallback({ type: TransactionStateStatus.SUCCESS, error: undefined });
+            return { web3Provider, signer };
+        } catch (e) {
+            error(`Error in metamask : ${e}`);
+            setDispatchCallback({ type: TransactionStateStatus.ERROR, message: e.message });
+        }
+    }
+
+    const provider  = useMemo<GetProvider | undefined>(getProvider, []);
+    return { state, web3Provider: provider?.web3Provider, signer: provider?.signer };
+};
