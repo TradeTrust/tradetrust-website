9c75e9fa73c6f42cf3420c93b7e9251ac0681122
Raymond Yeh <ray@geek.sg>
2020-03-20 16:18:31 +0800

    fix: lint

--- src/common/hooks/useAddressBook.ts
+++ src/common/hooks/useAddressBook.ts
@@ -10,7 +10,7 @@ export const useAddressBook = () => {
   // Recasting the dispatch action as the @types/use-persisted-state does not provide generics to set type
   const addressBook: AddressBook | undefined = rawAddressBook;
   const setAddressBook: Dispatch<SetStateAction<AddressBook>> = rawSetAddressBook as Dispatch<SetStateAction<unknown>>;
-  
+
   const getIdentifier = (address: string) => addressBook && addressBook[address.toLowerCase()];
   return { addressBook, setAddressBook, getIdentifier };
 };
