b252b7a5399fe447df8f710e1286fe2c7e31181d
Raymond Yeh <ray@geek.sg>
2020-03-20 16:06:48 +0800

    fix: recasting type to unknown before fixing it

--- src/common/hooks/useAddressBook.ts
+++ src/common/hooks/useAddressBook.ts
@@ -7,8 +7,10 @@ export interface AddressBook {
 
 export const useAddressBook = () => {
   const [rawAddressBook, rawSetAddressBook] = createPersistedState("ADDRESS_BOOK")();
+  // Recasting the dispatch action as the @types/use-persisted-state does not provide generics to set type
   const addressBook: AddressBook | undefined = rawAddressBook;
-  const setAddressBook: Dispatch<SetStateAction<AddressBook | undefined>> = rawSetAddressBook;
+  const setAddressBook: Dispatch<SetStateAction<AddressBook>> = rawSetAddressBook as Dispatch<SetStateAction<unknown>>;
+  
   const getIdentifier = (address: string) => addressBook && addressBook[address.toLowerCase()];
   return { addressBook, setAddressBook, getIdentifier };
 };
