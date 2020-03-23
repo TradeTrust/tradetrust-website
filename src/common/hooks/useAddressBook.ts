0e76bce113947827ce12d052c3bb44d8195755c9
Raymond Yeh <ray@geek.sg>
2020-03-23 10:45:00 +0800

    fix: address book default value & typing

--- src/common/hooks/useAddressBook.ts
+++ src/common/hooks/useAddressBook.ts
@@ -1,16 +1,12 @@
 import createPersistedState from "use-persisted-state";
-import { Dispatch, SetStateAction } from "react";
 
 export interface AddressBook {
   [key: string]: string;
 }
 
 export const useAddressBook = () => {
-  const [rawAddressBook, rawSetAddressBook] = createPersistedState("ADDRESS_BOOK")();
-  // Recasting the dispatch action as the @types/use-persisted-state does not provide generics to set type
-  const addressBook: AddressBook | undefined = rawAddressBook;
-  const setAddressBook: Dispatch<SetStateAction<AddressBook>> = rawSetAddressBook as Dispatch<SetStateAction<unknown>>;
-
+  const defaultAddressBook: AddressBook = {};
+  const [addressBook, setAddressBook] = createPersistedState("ADDRESS_BOOK")(defaultAddressBook);
   const getIdentifier = (address: string) => addressBook && addressBook[address.toLowerCase()];
   return { addressBook, setAddressBook, getIdentifier };
 };
