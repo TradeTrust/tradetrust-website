bfc345273fa05a4a4ee33f5e889d0bc8feb6b19e
RJ <me@rjchow.com>
2020-03-27 13:34:02 +0800

    chore: delete combined hash
    Unused utility that imports keccak causing long build times

--- src/utils/index.test.js
+++ src/utils/index.test.js
@@ -1,12 +1,4 @@
-import { combinedHash, isEthereumAddress } from "./index";
-
-describe("combinedHash", () => {
-  it("should work with two strings", () => {
-    expect(combinedHash("foo", "bar").hexSlice()).toBe(
-      "24e03926e272d3112eace88e8d04303e287d51571475e2334346dcc69f21041f"
-    );
-  });
-});
+import { isEthereumAddress } from "./index";
 
 describe("isEthereumAddress", () => {
   it("should return true on a valid ethereum address", () => {
