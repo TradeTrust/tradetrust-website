66c6fd0c71a7a6a32758b4b7e11ed1590aeab4f0
RJ <27843274+rjchow@users.noreply.github.com>
2020-04-01 00:11:18 +0800

    chore: removed unnecessary mock in DetailedCertificateVerifyBlock.test (#112)
    test contained unnecessary code and parameters

--- src/components/CertificateVerifyBlock/DetailedCertificateVerifyBlock/DetailedCertificateVerifyBlock.test.js
+++ src/components/CertificateVerifyBlock/DetailedCertificateVerifyBlock/DetailedCertificateVerifyBlock.test.js
@@ -2,20 +2,12 @@ import React from "react";
 import { mount } from "enzyme";
 import { act } from "react-dom/test-utils";
 import DetailedCertificateVerifyBlock from ".";
-import sampleToken from "../../../test/fixture/sample-token.json";
-
-jest.mock("../../../services/token", () => ({
-  ...jest.requireActual("../../../services/token"),
-}));
 
 describe("detailedCertificateVerifyBlock", () => {
-  afterEach(() => {
-    jest.restoreAllMocks();
-  });
   it("should render when document is token", async () => {
     let wrapper;
     await act(async () => {
-      wrapper = mount(<DetailedCertificateVerifyBlock verificationStatus={{ valid: true }} document={sampleToken} />);
+      wrapper = mount(<DetailedCertificateVerifyBlock verificationStatus={{ valid: true }} />);
     });
     wrapper.setProps();
     expect(wrapper.find("h5").text()).toStrictEqual("Details");
