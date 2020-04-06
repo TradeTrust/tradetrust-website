8f5891703f3c30137b0f00fcfb16e2d47ce75548
Sim Boon Long <simboonlong@gmail.com>
2020-04-06 14:13:03 +0800

    chore: styled-components with emotion (#114)
    - rename button naming variables to make more sense

---
+++ src/styles/abstracts/mixin.tsx
@@ -0,0 +1,59 @@
+export const fontMontserratRegular = () => {
+  return `
+    font-family: "Montserrat", Helvetica, Arial, sans-serif;
+    font-weight: 400;
+  `;
+};
+
+export const fontMontserratMedium = () => {
+  return `
+    font-family: "Montserrat", Helvetica, Arial, sans-serif;
+    font-weight: 500;
+  `;
+};
+
+export const fontMontserratSemibold = () => {
+  return `
+    font-family: "Montserrat", Helvetica, Arial, sans-serif;
+    font-weight: 600;
+  `;
+};
+
+export const fontSourcesansproRegular = () => {
+  return `
+    font-family: "Source Sans Pro", Helvetica, Arial, sans-serif;
+    font-weight: 400;
+  `;
+};
+
+export const fontSourcesansproBold = () => {
+  return `
+    font-family: "Source Sans Pro", Helvetica, Arial, sans-serif;
+    font-weight: 700;
+  `;
+};
+
+const pxToRem = (size: number, base = 16) => {
+  return (size / base) * 1 + "rem";
+};
+
+/*
+The mixin fontSize(size) is to have a fallback to px value, if rem is somehow not supported in IE. it will look like these:
+
+supported:
+font-size: 16px; (Strikethrough, succeeding font-size overwrites)
+font-size: 1rem;
+
+not supported:
+font-size: 16px;
+font-size: 1rem; (Strikethrough, preceding font-size used)
+
+The default value will be 16px, if size is not defined.
+*/
+
+export const fontSize = (size = 16) => {
+  return `
+    font-size: ${size}px;
+    font-size: ${pxToRem(size)};
+  `;
+};
---
+++ src/styles/abstracts/variables.tsx
@@ -0,0 +1,38 @@
+export const brandOrange = "#ff9933";
+export const brandBlue = "#0099cc";
+export const brandNavy = "#324353";
+
+export const blueLightest = "#f5f8fb";
+export const blueLighter = "#f3f8fc";
+export const blue = "#099de3";
+export const greyblue = "#c1c9d1";
+export const greyblueDark = "#a7afb7";
+export const greyblueDarker = "#343a40";
+export const greenLightest = "#f5fbf7";
+export const green = "#00c04a";
+export const orange = "#ffb152";
+export const redLighter = "#fbeae9";
+export const red = "#ff5268";
+export const redDark = "#8b0000";
+export const greyLightest = "#f7fafc";
+export const greyLighter = "#e5e5e5";
+export const greyLight = "#dddddd";
+export const grey = "#8f8f8f";
+export const greyDark = "#5a5a5a";
+export const white = "#ffffff";
+export const black = "#000000";
+
+export const success = "#28a745";
+export const danger = "#c12323";
+export const warning = "#ffbf00";
+export const invalid = "#e46767";
+
+export const buttonRadius = "4px";
+export const buttonOutline = brandNavy;
+export const roundButtonOutline = brandNavy;
+
+export const maxWidth = "1280px";
+
+export const easeInCubic = "cubic-bezier(0.55, 0.055, 0.675, 0.19)";
+export const easeOutCubic = "cubic-bezier(0.215, 0.61, 0.355, 1)";
+export const easeInOutCubic = "cubic-bezier(0.645, 0.045, 0.355, 1)";
