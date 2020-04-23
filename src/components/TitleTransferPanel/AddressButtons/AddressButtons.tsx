972570c8f053773f3f8486a9db0394b2d01f896c
Sumit Chaudhari <sumit.nitsurat@gmail.com>
2020-04-23 14:01:49 +0800

    temp

--- src/components/TitleTransferPanel/AddressButtons/AddressButtons.tsx
+++ src/components/TitleTransferPanel/AddressButtons/AddressButtons.tsx
@@ -1,50 +1,27 @@
 import React from "react";
 import css from "./AddressButtons.module.scss";
-import { SvgIcon, SvgIconBook, SvgIconX } from "../../UI/SvgIcon";
-import { ButtonSolidWhiteOrange, ButtonSolidOrange, ButtonCircleGreylight } from "../../UI/Button";
+import { SvgIcon, SvgIconBook, } from "../../UI/SvgIcon";
+import { ButtonSolidWhiteOrange, } from "../../UI/Button";
 
 interface AddressButtonsProps {
-  holderAddress: string;
-  beneficiaryAddress?: string;
   isDisabledAddressBook?: boolean;
-  buttonText: string;
-  onButtonSubmit?(event: React.MouseEvent<HTMLButtonElement>): void;
 }
 
 export const AddressButtons = ({
-  holderAddress,
-  beneficiaryAddress,
   isDisabledAddressBook,
-  buttonText,
-  onButtonSubmit,
 }: AddressButtonsProps) => {
-  const isSubmitEnabled =
-    beneficiaryAddress === undefined
-      ? holderAddress.length === 0
-      : holderAddress.length === 0 || beneficiaryAddress.length === 0;
 
   return (
     <div className={css["address-cta"]}>
       <ButtonSolidWhiteOrange disabled={isDisabledAddressBook}>
         <div className="row no-gutters align-items-center">
-          <div className="col-auto mr-2">
+          <div className="col-auto">
             <SvgIcon>
               <SvgIconBook />
             </SvgIcon>
           </div>
-          <div className="col">
-            <p>Address</p>
-          </div>
         </div>
       </ButtonSolidWhiteOrange>
-      <ButtonSolidOrange disabled={isSubmitEnabled} onClick={onButtonSubmit}>
-        {buttonText}
-      </ButtonSolidOrange>
-      <ButtonCircleGreylight>
-        <SvgIcon>
-          <SvgIconX />
-        </SvgIcon>
-      </ButtonCircleGreylight>
     </div>
   );
 };
