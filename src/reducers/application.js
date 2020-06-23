f4b38c41526a9bd2ee435c826b843b0e94561b63
RJ <27843274+rjchow@users.noreply.github.com>
2020-06-23 13:47:24 +0800

    chore: removed dead code for polling tx (#167)
    removed some very legacy code for polling Infura from before we used ethersjs

--- src/reducers/application.js
+++ src/reducers/application.js
@@ -1,15 +1,9 @@
-import { omit } from "lodash";
 import { DEFAULT_NETWORK } from "../config";
 
 export const initialState = {
   network: DEFAULT_NETWORK,
   networkId: null,
   networkIdVerbose: "",
-  customRpc: "",
-  networkUpdatePending: true,
-  txPollingList: {},
-  currentBlockContents: undefined,
-  currentBlockNumber: 0,
 };
 
 // Actions
@@ -18,18 +12,6 @@ export const types = {
   UPDATE_NETWORK_ID: "UPDATE_NETWORK_ID",
   UPDATE_NETWORK_ID_SUCCESS: "UPDATE_NETWORK_ID_SUCCESS",
   UPDATE_NETWORK_ID_FAILURE: "UPDATE_NETWORK_ID_FAILURE",
-
-  NEW_BLOCK: "NEW_BLOCK",
-
-  TRANSACTION_MINED: "TRANSACTION_MINED",
-
-  TX_POLLING_ADD: "TX_POLLING_ADD",
-  TX_POLLING_REMOVE: "TX_POLLING_REMOVE",
-
-  // polling_started
-  // add to poll : should take a tx hash and a callback action
-  // remove from poll
-  // polling_stopped
 };
 
 // Reducers
@@ -39,7 +21,6 @@ export default function reducer(state = initialState, action) {
       return {
         ...state,
         network: action.payload.network,
-        customRpc: action.payload.customRpc,
       };
     case types.UPDATE_NETWORK_ID:
       return {
@@ -64,31 +45,6 @@ export default function reducer(state = initialState, action) {
         networkIdVerbose: "",
         networkUpdatePending: false,
       };
-    case types.TX_POLLING_ADD:
-      return {
-        ...state,
-        txPollingList: { ...state.txPollingList, [action.payload.txHash]: true },
-      };
-    case types.TX_POLLING_REMOVE:
-      return {
-        ...state,
-        txPollingList: omit(state.txPollingList, action.payload.txHash),
-      };
-
-    case types.TRANSACTION_MINED:
-      return {
-        ...state,
-        minedTransactions: {
-          ...state.minedTransactions,
-          [action.payload.txHash]: action.payload.txReceipt,
-        },
-      };
-    case types.NEW_BLOCK:
-      return {
-        ...state,
-        currentBlockNumber: action.payload.blockNumber,
-        currentBlockContents: action.payload.blockContents,
-      };
     default:
       return state;
   }
@@ -108,27 +64,6 @@ export function updateNetworkId() {
   };
 }
 
-export function foundNewBlock(payload) {
-  return {
-    type: types.NEW_BLOCK,
-    payload,
-  };
-}
-
-export function announceMinedTransaction(payload) {
-  return {
-    type: types.TRANSACTION_MINED,
-    payload,
-  };
-}
-
-export function removeTxFromPollingList(payload) {
-  return {
-    type: types.TX_POLLING_REMOVE,
-    payload,
-  };
-}
-
 // Selectors
 export function getNetwork(store) {
   return store.application.network;
@@ -138,29 +73,6 @@ export function getNetworkPending(store) {
   return store.application.networkUpdatePending;
 }
 
-export function getCustomRpc(store) {
-  return store.application.customRpc;
-}
-
 export function getNetworkId(store) {
   return store.application.networkId;
-}
-
-export function getTxPollingList(store) {
-  return store.application.txPollingList;
-}
-
-export function getNetworkPollingTask(store) {
-  return store.application.networkPollingTask;
-}
-
-export function getCurrentBlockNumber(store) {
-  return store.application.currentBlockNumber;
-}
-
-export function getTransactionReceipt(store, txHash) {
-  if (store.application.minedTransactions[txHash]) {
-    return store.application.minedTransactions[txHash];
-  }
-  return undefined;
 }
