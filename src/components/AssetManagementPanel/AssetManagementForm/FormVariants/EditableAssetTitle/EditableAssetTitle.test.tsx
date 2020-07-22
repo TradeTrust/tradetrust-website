c6ad3d0ed44947ab2ae62cb603b14e5b5a82f2ab
Sim Boon Long <simboonlong@gmail.com>
2020-07-22 15:25:58 +0800

    chore: upgrade decentralize-renderer-component to v3

--- src/components/AssetManagementPanel/AssetManagementForm/FormVariants/EditableAssetTitle/EditableAssetTitle.test.tsx
+++ src/components/AssetManagementPanel/AssetManagementForm/FormVariants/EditableAssetTitle/EditableAssetTitle.test.tsx
@@ -1,17 +1,20 @@
 import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
+import { render, act } from "@testing-library/react";
 import React from "react";
 import { EditableAssetTitle } from "./EditableAssetTitle";
 
-it("should display both the role and the value when not in editable format", () => {
-  const container = render(
-    <EditableAssetTitle role="Beneficiary" value="0xE94E4f16ad40ADc90C29Dc85b42F1213E034947C" isEditable={false} />
-  );
-  const beneficiaryLabel = container.getByText("Beneficiary:");
-  const beneficiaryText = container.getByText("0xE94E4f16ad40ADc90C29Dc85b42F1213E034947C");
+it("should display both the role and the value when not in editable format", async () => {
+  await act(async () => {
+    const container = render(
+      <EditableAssetTitle role="Beneficiary" value="0xE94E4f16ad40ADc90C29Dc85b42F1213E034947C" isEditable={false} />
+    );
 
-  expect(beneficiaryLabel).not.toBeNull();
-  expect(beneficiaryText).not.toBeNull();
+    const beneficiaryLabel = container.getByText("Beneficiary:");
+    const beneficiaryText = container.getByText("0xE94E4f16ad40ADc90C29Dc85b42F1213E034947C");
+
+    expect(beneficiaryLabel).not.toBeNull();
+    expect(beneficiaryText).not.toBeNull();
+  });
 });
 
 it("should display both the role and the editable field when in editable format", async () => {
