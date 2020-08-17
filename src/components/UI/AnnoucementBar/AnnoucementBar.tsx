508b33cbc70418653df9ecb6168b710ba1de9853
Sim Boon Long <simboonlong@gmail.com>
2020-08-17 14:47:09 +0800

    fix: lint

--- src/components/UI/AnnoucementBar/AnnoucementBar.tsx
+++ src/components/UI/AnnoucementBar/AnnoucementBar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "@emotion/styled";
-import { vars, mixin } from "../../../styles";
+import { vars } from "../../../styles";
 import { ReactRouterLinkButtonSolidOrangeWhite } from "./../../UI/Button";
 
 export interface AnnoucementBarProps {
