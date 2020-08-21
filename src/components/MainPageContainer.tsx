f224e9eb7235bfb33bf0a93ae965ef2667daaf6c
Sim Boon Long <simboonlong@gmail.com>
2020-08-21 10:16:48 +0800

    chore: remove webinar banner

--- src/components/MainPageContainer.tsx
+++ src/components/MainPageContainer.tsx
@@ -4,12 +4,10 @@ import { MainBenefitsSection } from "./HomePageContent/MainBenefitsSection";
 import { DocumentationSection } from "./HomePageContent/DocumentationSection";
 import { EmailSection } from "./HomePageContent/EmailSection";
 import { DropZoneSectionContainer } from "./HomePageContent/DropZoneSection";
-import { AnnoucementBar } from "./UI/AnnoucementBar";
 
 const MainPageContainer = () => {
   return (
     <>
-      <AnnoucementBar />
       <DropZoneSectionContainer />
       <LandingSection />
       <MainBenefitsSection />
