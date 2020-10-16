4e337007b26bbc6efc4568d738f4ccf4ee621713
justussoh <justus.soh@hotmail.com>
2020-10-16 13:03:56 +0800

    fix: rename and split document retrieval methods

--- src/services/qrProcessor/index.js
+++ src/services/qrProcessor/index.js
@@ -1,10 +1,14 @@
 import axios from "axios";
 import { decryptString } from "@govtechsg/oa-encryption";
+import { getLogger } from "../../utils/logger";
+
+const { error } = getLogger("services:qrProcessor");
 
 export const decodeQrCode = (qrCode) => {
-  const ttRegex = /https:\/\/action.openattestation.com\/\?q=(.*)/;
-  if (!ttRegex.test(qrCode)) throw new Error("QR Code is not formatted to TradeTrust specifications");
-  const [, encodedPayload] = ttRegex.exec(qrCode);
+  const openAttestationActionRegex = /https:\/\/action.openattestation.com\/\?q=(.*)/;
+  if (!openAttestationActionRegex.test(qrCode))
+    throw new Error("QR Code is not formatted to TradeTrust specifications");
+  const [, encodedPayload] = openAttestationActionRegex.exec(qrCode);
   const decodedPayload = JSON.parse(decodeURIComponent(encodedPayload));
   return decodedPayload;
 };
@@ -12,17 +16,18 @@ export const decodeQrCode = (qrCode) => {
 export const encodeQrCode = (payload) =>
   `https://action.openattestation.com/?q=${encodeURIComponent(JSON.stringify(payload))}`;
 
-const decryptQuery = async (action) => {
-  if (action.type !== "DOCUMENT") throw new Error(`The type ${action.type} provided from the action is not supported`);
-  const { uri, key } = action.payload;
-  const response = await axios.get(uri);
-  if (response.status >= 400 && response.status < 600) throw new Error(`Unable to load the certificate from ${uri}`);
-  let certificate = response.data;
-  certificate = certificate.document || certificate; // opencerts-function returns the document in a nested document object
-
-  if (!certificate) {
-    throw new Error(`Certificate at address ${uri} is empty`);
+export const getDocumentFromUri = async (uri) => {
+  try {
+    const response = await axios.get(uri);
+    let certificate = response.data;
+    certificate = certificate.document || certificate; // opencerts-function returns the document in a nested document object
+    return certificate;
+  } catch (e) {
+    throw new Error(`Unable to load the certificate from ${uri}`);
   }
+};
+
+const decryptDocument = async (certificate, key) => {
   // if there is a key and the type is "OPEN-ATTESTATION-TYPE-1", let's use oa-encryption
   if (key && certificate.type === "OPEN-ATTESTATION-TYPE-1") {
     certificate = JSON.parse(
@@ -42,10 +47,17 @@ const decryptQuery = async (action) => {
 
 export const processQrCode = async (qrCode) => {
   try {
-    const queryParam = decodeQrCode(qrCode);
-    const data = await decryptQuery(queryParam);
+    const action = decodeQrCode(qrCode);
+    if (action.type !== "DOCUMENT")
+      throw new Error(`The type ${action.type} provided from the action is not supported`);
+    const certificate = await getDocumentFromUri(action.payload.uri);
+    if (!certificate) {
+      throw new Error(`Certificate at address ${uri} is empty`);
+    }
+    const data = await decryptDocument(certificate, action.payload.key);
     return data;
   } catch (e) {
+    error(e.message);
     throw new Error(e.message);
   }
 };
