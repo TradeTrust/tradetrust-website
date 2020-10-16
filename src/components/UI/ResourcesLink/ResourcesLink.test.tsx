0685c8abc728e20098eb08d86e45e70bdfe3c110
Matthea Loo <dhemutton@users.noreply.github.com>
2020-10-16 08:20:04 +0800

    fix: fix resources link test

--- src/components/UI/ResourcesLink/ResourcesLink.test.tsx
+++ src/components/UI/ResourcesLink/ResourcesLink.test.tsx
@@ -6,11 +6,13 @@ import { ResourcesLink } from "./ResourcesLink";
 const mockInfo = {
   title: "Source code",
   type: "link",
-  details: {
-    description: "View TradeTrust source code",
-    url: "https://github.com/TradeTrust/tradetrust-website",
-    icon: "/static/images/Github_Octocat.png",
-  },
+  details: [
+    {
+      description: "View TradeTrust source code",
+      url: "https://github.com/TradeTrust/tradetrust-website",
+    },
+  ],
+  icon: "/static/images/Github_Octocat.png",
 } as ResourcesLinkProps;
 
 describe("ResourcesLink", () => {
@@ -21,21 +23,25 @@ describe("ResourcesLink", () => {
   });
 
   it("should render redirect link correctly", () => {
-    render(<ResourcesLink title={mockInfo.title} type={mockInfo.type} details={mockInfo.details} />);
+    render(
+      <ResourcesLink title={mockInfo.title} type={mockInfo.type} details={mockInfo.details} icon={mockInfo.icon} />
+    );
 
     expect(screen.queryAllByTestId("link")).toHaveLength(1);
     expect(screen.queryAllByTestId("download")).toHaveLength(0);
   });
 
   it("should render download link correctly", () => {
-    render(<ResourcesLink title={mockInfo.title} type="download" details={mockInfo.details} />);
+    render(<ResourcesLink title={mockInfo.title} type="download" details={mockInfo.details} icon={mockInfo.icon} />);
 
     expect(screen.queryAllByTestId("link")).toHaveLength(0);
     expect(screen.queryAllByTestId("download")).toHaveLength(1);
   });
 
   it("should render icon if there are any icon", () => {
-    render(<ResourcesLink title={mockInfo.title} type={mockInfo.type} details={mockInfo.details} />);
+    render(
+      <ResourcesLink title={mockInfo.title} type={mockInfo.type} details={mockInfo.details} icon={mockInfo.icon} />
+    );
 
     expect(screen.queryAllByTestId("link-icon")).toHaveLength(1);
   });
