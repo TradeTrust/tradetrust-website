3be451eb8057c363012906e3d2eff881f4f0efe8
Sim Boon Long <simboonlong@gmail.com>
2020-10-19 13:47:51 +0800

    chore: try without ci package

--- integration/local/index.mjs
+++ integration/local/index.mjs
@@ -8,7 +8,7 @@ const main = async () => {
     executablePath: process.env.PUPPETEER_EXEC_PATH, // set by docker container
     headless: false,
     defaultViewport: null,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    args: ["--no-sandbox", "--disable-setuid-sandbox"], // https://github.com/puppeteer/puppeteer/blob/main/docs/troubleshooting.md#setting-up-chrome-linux-sandbox
   });
   const metamask = await dappeteer.getMetamask(browser, {
     seed: "indicate swing place chair flight used hammer soon photo region volume shuffle",
