b63698ac06a894bfc8e0a3f2e5d190e85b342ccf
justussoh <justus.soh@hotmail.com>
2020-10-29 12:22:21 +0800

    fix: moved comment above before import

--- src/components/QrReader/Scanner.js
+++ src/components/QrReader/Scanner.js
@@ -1,7 +1,7 @@
 import { useLayoutEffect } from "react";
 import QrScanner from "qr-scanner";
-import QrScannerWorkerPath from "!!file-loader!../../../node_modules/qr-scanner/qr-scanner-worker.min.js";
 // `!!file-loader!` explicitly instruct webpack to import file with file-loader overwriting global config
+import QrScannerWorkerPath from "!!file-loader!../../../node_modules/qr-scanner/qr-scanner-worker.min.js";
 QrScanner.WORKER_PATH = QrScannerWorkerPath;
 
 export const useScanner = ({ onDetected, scannerRef }) => {
