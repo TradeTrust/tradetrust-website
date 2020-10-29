3002a8cdc8f2c39d547fc08fb1ff14bffe00263c
Justus Soh <45728710+justussoh@users.noreply.github.com>
2020-10-29 14:49:37 +0800

    fix: clean up action.openattestation trailing slash and git repository warning (#263)
    * fix: clean up echain tbody in div warning
    
    * fix: standardise qrcode uri
    
    * fix: moved comment above before import
    
    * chore: added repo link to package.json

--- src/components/QrReader/Scanner.js
+++ src/components/QrReader/Scanner.js
@@ -1,5 +1,6 @@
 import { useLayoutEffect } from "react";
 import QrScanner from "qr-scanner";
+// `!!file-loader!` explicitly instruct webpack to import file with file-loader overwriting global config
 import QrScannerWorkerPath from "!!file-loader!../../../node_modules/qr-scanner/qr-scanner-worker.min.js";
 QrScanner.WORKER_PATH = QrScannerWorkerPath;
 
