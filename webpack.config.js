4ed4b13434139f6760f1e1be486d2808b18b79cf
Sim Boon Long <simboonlong@gmail.com>
2020-11-10 14:41:27 +0800

    fix: load styles

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,10 @@ module.exports = {
           loader: "babel-loader",
         },
       },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
     ],
   },
   plugins: [
