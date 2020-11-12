c01e4d3994fbe1cdf2148decd710aa1ea5b0435f
Sim Boon Long <simboonlong@gmail.com>
2020-11-12 14:35:02 +0800

    feat: migrate tailwind

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    process.env.NODE_ENV === "production" &&
+      require("@fullhuman/postcss-purgecss")({
+        content: ["./src/**/*.tsx", "./public/index.html"],
+        defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+      }),
+  ],
+};
