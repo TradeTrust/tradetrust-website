d4d235b97b4b7d0332c9b101c9cdfcd70211f3f9
Matthea Loo <dhemutton@users.noreply.github.com>
2020-12-01 14:11:03 +0800

    fix: remove unused test id

--- src/components/TabPaneAttachments/TabPaneAttachments.tsx
+++ src/components/TabPaneAttachments/TabPaneAttachments.tsx
@@ -14,7 +14,7 @@ export const TabPaneAttachments = ({ attachments }: TabPaneAttachmentsProps) => 
     <div className="container">
       <div className="flex flex-wrap -mx-4">
         {attachments.map(({ filename, data, type }, index) => (
-          <div className="w-1/2 lg:w-1/3 xl:w-1/4 mb-3 px-4" key={index} data-testid={`attachment-${index}`}>
+          <div className="w-1/2 lg:w-1/3 xl:w-1/4 mb-3 px-4" key={index}>
             <AttachmentLink filename={filename} data={data} type={type} />
           </div>
         ))}
