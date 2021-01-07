dcb34f50c66db400a880a77dfd9a848b19e997b4
isaac <isaac.kps@gmail.com>
2021-01-07 15:52:55 +0800

    fix: test case

--- src/components/EndorsementChain/EndorsementChainLayout/EndorsementChainLayout.test.tsx
+++ src/components/EndorsementChain/EndorsementChainLayout/EndorsementChainLayout.test.tsx
@@ -81,7 +81,7 @@ describe("EndorsementChainLayout", () => {
         setShowEndorsementChain={() => {}}
       />
     );
-    expect(screen.getAllByText("Document surrendered to Issuer")).toHaveLength(1);
+    expect(screen.getAllByText("Document surrendered to issuer")).toHaveLength(1);
   });
 
   it("should render text Surrendered if burnt event is present", () => {
