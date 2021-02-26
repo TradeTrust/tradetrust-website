72972edd3d0926158f37dbb88d9bb514de85e60b
Sim Boon Long <simboonlong@gmail.com>
2021-02-26 08:53:56 +0800

    feat: storybook v6 (#316)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,9 @@
 module.exports = {
-  stories: [
-    '../src/**/*.stories.(mdx|tsx)',
-  ],
+  stories: ["../src/**/*.stories.@(mdx|tsx)"],
   addons: [
-    '@storybook/preset-create-react-app',
-    '@storybook/addon-docs',
-    '@storybook/addon-actions',
-    '@storybook/addon-knobs'
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-docs",
+    "@storybook/addon-actions",
+    "@storybook/addon-knobs",
   ],
 };
