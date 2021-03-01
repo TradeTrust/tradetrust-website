91b2f8913373a59c67ef9e4f299c0af2415067fa
Dimple-Kanwar <dimplekanwar.dks@gmail.com>
2021-03-01 23:29:42 +0530

    Added custom verifier and test cases

--- src/services/verify/fragments.test.tsx
+++ src/services/verify/fragments.test.tsx
@@ -11,78 +11,184 @@ import {
   whenDocumentIssuerIdentityInvalidDid,
   whenTransferableDocumentVerified,
 } from "../../test/fixture/verifier-responses";
+import { NETWORK_NAME } from "../../config";
+import {
+  whenDocumentHashInvalidAndNotIssuedCorda,
+  whenDocumentHashInvalidCorda,
+  whenDocumentIssuerIdentityInvalidDidCorda,
+  whenDocumentIssuerIdentityInvalidDnsTxtCorda,
+  whenDocumentNotIssuedCorda,
+  whenDocumentRevokedCorda,
+  whenDocumentValidAndIssuedByDidCorda,
+  whenDocumentValidAndIssuedByDnsCorda,
+  whenTransferableDocumentVerifiedCorda,
+} from "../../test/fixture/corda-verifier-responses";
 
 describe("interpretFragments", () => {
-  it("should interpret whenDocumentHashInvalidAndNotIssued correctly", () => {
-    expect(interpretFragments(whenDocumentHashInvalidAndNotIssued as VerificationFragment[])).toEqual({
-      hashValid: false,
-      issuedValid: false,
-      identityValid: false,
-      revokedValid: false,
+  if (NETWORK_NAME === "Corda Enterprise") {
+    // Corda test cases
+    it("should interpret whenDocumentHashInvalidAndNotIssuedCorda correctly", () => {
+      expect(interpretFragments(whenDocumentHashInvalidAndNotIssuedCorda as VerificationFragment[])).toEqual({
+        consumedValid: false,
+        hashValid: false,
+        issuedValid: false,
+        identityValid: false,
+        revokedValid: false,
+      });
     });
-  });
-  it("should interpret whenDocumentNotIssued correctly", () => {
-    expect(interpretFragments(whenDocumentNotIssued as VerificationFragment[])).toEqual({
-      hashValid: true,
-      issuedValid: false,
-      identityValid: true,
-      revokedValid: true,
+    it("should interpret whenDocumentNotIssuedCorda correctly", () => {
+      expect(interpretFragments(whenDocumentNotIssuedCorda as VerificationFragment[])).toEqual({
+        consumedValid: false,
+        hashValid: true,
+        issuedValid: false,
+        identityValid: true,
+        revokedValid: false,
+      });
     });
-  });
-  it("should interpret whenDocumentValidAndIssuedByDns correctly", () => {
-    expect(interpretFragments(whenDocumentValidAndIssuedByDns as VerificationFragment[])).toEqual({
-      hashValid: true,
-      issuedValid: true,
-      identityValid: true,
-      revokedValid: true,
+    it("should interpret whenDocumentValidAndIssuedByDnsCorda correctly", () => {
+      expect(interpretFragments(whenDocumentValidAndIssuedByDnsCorda as VerificationFragment[])).toEqual({
+        consumedValid: true,
+        hashValid: true,
+        issuedValid: true,
+        identityValid: true,
+        revokedValid: true,
+      });
     });
-  });
-  it("should interpret whenDocumentValidAndIssuedByDid correctly", () => {
-    expect(interpretFragments(whenDocumentValidAndIssuedByDid as VerificationFragment[])).toEqual({
-      hashValid: true,
-      issuedValid: true,
-      identityValid: true,
-      revokedValid: true,
+    it("should interpret whenDocumentValidAndIssuedByDidCorda correctly", () => {
+      expect(interpretFragments(whenDocumentValidAndIssuedByDidCorda as VerificationFragment[])).toEqual({
+        consumedValid: true,
+        hashValid: true,
+        issuedValid: true,
+        identityValid: false,
+        revokedValid: true,
+      });
     });
-  });
-  it("should interpret whenDocumentHashInvalid correctly", () => {
-    expect(interpretFragments(whenDocumentHashInvalid as VerificationFragment[])).toEqual({
-      hashValid: false,
-      issuedValid: true,
-      identityValid: true,
-      revokedValid: true,
+    it("should interpret whenDocumentHashInvalidCorda correctly", () => {
+      expect(interpretFragments(whenDocumentHashInvalidCorda as VerificationFragment[])).toEqual({
+        consumedValid: true,
+        hashValid: false,
+        issuedValid: true,
+        identityValid: true,
+        revokedValid: true,
+      });
     });
-  });
-  it("should interpret whenDocumentRevoked correctly", () => {
-    expect(interpretFragments(whenDocumentRevoked as VerificationFragment[])).toEqual({
-      hashValid: true,
-      issuedValid: true,
-      identityValid: true,
-      revokedValid: false,
+    it("should interpret whenDocumentRevokedCorda correctly", () => {
+      expect(interpretFragments(whenDocumentRevokedCorda as VerificationFragment[])).toEqual({
+        consumedValid: false,
+        hashValid: true,
+        issuedValid: true,
+        identityValid: true,
+        revokedValid: true,
+      });
     });
-  });
-  it("should interpret whenDocumentIssuerIdentityInvalidDnsTxt correctly", () => {
-    expect(interpretFragments(whenDocumentIssuerIdentityInvalidDnsTxt as VerificationFragment[])).toEqual({
-      hashValid: true,
-      issuedValid: true,
-      identityValid: false,
-      revokedValid: true,
+    it("should interpret whenDocumentIssuerIdentityInvalidDnsTxtCorda correctly", () => {
+      expect(interpretFragments(whenDocumentIssuerIdentityInvalidDnsTxtCorda as VerificationFragment[])).toEqual({
+        consumedValid: true,
+        hashValid: true,
+        issuedValid: true,
+        identityValid: false,
+        revokedValid: true,
+      });
     });
-  });
-  it("should interpret whenDocumentIssuerIdentityInvalidDid correctly", () => {
-    expect(interpretFragments(whenDocumentIssuerIdentityInvalidDid as VerificationFragment[])).toEqual({
-      hashValid: true,
-      issuedValid: true,
-      identityValid: false,
-      revokedValid: true,
+    it("should interpret whenDocumentIssuerIdentityInvalidDidCorda correctly", () => {
+      expect(interpretFragments(whenDocumentIssuerIdentityInvalidDidCorda as VerificationFragment[])).toEqual({
+        consumedValid: true,
+        hashValid: true,
+        issuedValid: true,
+        identityValid: false,
+        revokedValid: true,
+      });
     });
-  });
-  it("should interpret whenTransferableDocumentVerified correctly", () => {
-    expect(interpretFragments(whenTransferableDocumentVerified as VerificationFragment[])).toEqual({
-      hashValid: true,
-      issuedValid: true,
-      identityValid: true,
-      revokedValid: true,
+    it("should interpret whenTransferableDocumentVerifiedCorda correctly", () => {
+      expect(interpretFragments(whenTransferableDocumentVerifiedCorda as VerificationFragment[])).toEqual({
+        consumedValid: false,
+        hashValid: true,
+        issuedValid: false,
+        identityValid: true,
+        revokedValid: false,
+      });
     });
-  });
+  } else {
+    it("should interpret whenDocumentHashInvalidAndNotIssued correctly", () => {
+      expect(interpretFragments(whenDocumentHashInvalidAndNotIssued as VerificationFragment[])).toEqual({
+        hashValid: false,
+        issuedValid: false,
+        consumedValid: false,
+        identityValid: false,
+        revokedValid: false,
+      });
+    });
+    it("should interpret whenDocumentNotIssued correctly", () => {
+      expect(interpretFragments(whenDocumentNotIssued as VerificationFragment[])).toEqual({
+        hashValid: true,
+        issuedValid: false,
+        consumedValid: false,
+        identityValid: true,
+        revokedValid: true,
+      });
+    });
+    it("should interpret whenDocumentValidAndIssuedByDns correctly", () => {
+      expect(interpretFragments(whenDocumentValidAndIssuedByDns as VerificationFragment[])).toEqual({
+        hashValid: true,
+        issuedValid: true,
+        consumedValid: false,
+        identityValid: true,
+        revokedValid: true,
+      });
+    });
+    it("should interpret whenDocumentValidAndIssuedByDid correctly", () => {
+      expect(interpretFragments(whenDocumentValidAndIssuedByDid as VerificationFragment[])).toEqual({
+        hashValid: true,
+        issuedValid: true,
+        consumedValid: false,
+        identityValid: true,
+        revokedValid: true,
+      });
+    });
+    it("should interpret whenDocumentHashInvalid correctly", () => {
+      expect(interpretFragments(whenDocumentHashInvalid as VerificationFragment[])).toEqual({
+        hashValid: false,
+        issuedValid: true,
+        consumedValid: false,
+        identityValid: true,
+        revokedValid: true,
+      });
+    });
+    it("should interpret whenDocumentRevoked correctly", () => {
+      expect(interpretFragments(whenDocumentRevoked as VerificationFragment[])).toEqual({
+        hashValid: true,
+        issuedValid: true,
+        consumedValid: false,
+        identityValid: true,
+        revokedValid: false,
+      });
+    });
+    it("should interpret whenDocumentIssuerIdentityInvalidDnsTxt correctly", () => {
+      expect(interpretFragments(whenDocumentIssuerIdentityInvalidDnsTxt as VerificationFragment[])).toEqual({
+        hashValid: true,
+        issuedValid: true,
+        consumedValid: false,
+        identityValid: false,
+        revokedValid: true,
+      });
+    });
+    it("should interpret whenDocumentIssuerIdentityInvalidDid correctly", () => {
+      expect(interpretFragments(whenDocumentIssuerIdentityInvalidDid as VerificationFragment[])).toEqual({
+        hashValid: true,
+        issuedValid: true,
+        consumedValid: false,
+        identityValid: false,
+        revokedValid: true,
+      });
+    });
+    it("should interpret whenTransferableDocumentVerified correctly", () => {
+      expect(interpretFragments(whenTransferableDocumentVerified as VerificationFragment[])).toEqual({
+        hashValid: true,
+        issuedValid: true,
+        consumedValid: false,
+        identityValid: true,
+        revokedValid: true,
+      });
+    });
+  }
 });
--- src/services/verify/fragments.tsx
+++ src/services/verify/fragments.tsx
@@ -1,13 +1,23 @@
 import { isValid, VerificationFragment } from "@govtechsg/oa-verify";
+import { NETWORK } from "../../config";
 const getFirstFragmentFor = (fragments: VerificationFragment[], name: string) =>
   fragments.filter((status) => status.name === name)[0];
 
-const revokeFragmentName = "OpenAttestationEthereumDocumentStoreRevoked";
+const issuedFragmentName = "OpenAttestationDocumentIssued";
+const consumedFragmentName = "OpenAttestationDocumentConsumed";
+let revokeFragmentName: string;
+if (NETWORK === "Corda Enterprise") revokeFragmentName = "OpenAttestationDocumentRevoked";
+else revokeFragmentName = "OpenAttestationEthereumDocumentStoreRevoked";
+
+export const getIssuedFragment = (fragments: VerificationFragment[]) =>
+  fragments.filter((status) => status.name === issuedFragmentName && status.status !== "SKIPPED");
 export const getNotRevokeFragment = (fragments: VerificationFragment[]) =>
   fragments.filter((status) => status.name !== revokeFragmentName && status.status !== "SKIPPED");
 export const getRevokeFragment = (fragments: VerificationFragment[]) =>
   fragments.filter((status) => status.name === revokeFragmentName && status.status !== "SKIPPED");
 
+export const getConsumedFragment = (fragments: VerificationFragment[]) =>
+  fragments.filter((status) => status.name === consumedFragmentName && status.status !== "SKIPPED");
 // this function check if the reason of the error is that the document store or token registry is invalid
 export const addressInvalid = (fragments: VerificationFragment[]) => {
   const documentStoreIssuedFragment = getFirstFragmentFor(fragments, "OpenAttestationEthereumDocumentStoreIssued");
@@ -26,10 +36,20 @@ export const certificateNotIssued = (fragments: VerificationFragment[]) => {
 
 export const interpretFragments = (fragments: VerificationFragment[]) => {
   const notRevokeFragments = getNotRevokeFragment(fragments);
+  const issuedFragments = getIssuedFragment(fragments);
+  let issuedValid;
+  if (NETWORK === "Corda Enterprise")
+    issuedValid = issuedFragments.length > 0 ? isValid(issuedFragments, ["DOCUMENT_STATUS"]) : false;
+  else issuedValid = isValid(notRevokeFragments, ["DOCUMENT_STATUS"]);
+
+  const consumedFragments = getConsumedFragment(fragments);
   const revokeFragments = getRevokeFragment(fragments);
   const hashValid = isValid(fragments, ["DOCUMENT_INTEGRITY"]);
-  const issuedValid = isValid(notRevokeFragments, ["DOCUMENT_STATUS"]);
-  const revokedValid = revokeFragments.length === 0 || isValid(revokeFragments, ["DOCUMENT_STATUS"]);
+  const consumedValid = consumedFragments.length > 0 ? isValid(consumedFragments, ["DOCUMENT_STATUS"]) : false;
+  let revokedValid;
+  if (NETWORK === "Corda Enterprise")
+    revokedValid = revokeFragments.length > 0 ? !isValid(revokeFragments, ["DOCUMENT_STATUS"]) : false;
+  else revokedValid = revokeFragments.length === 0 || isValid(revokeFragments, ["DOCUMENT_STATUS"]);
   const identityValid = isValid(fragments, ["ISSUER_IDENTITY"]);
-  return { hashValid, issuedValid, identityValid, revokedValid };
+  return { hashValid, issuedValid, consumedValid, identityValid, revokedValid };
 };
