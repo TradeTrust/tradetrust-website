8eefb0bb779c46428bf1a942fee457312f4b4eac
isaac <isaac.kps@gmail.com>
2021-03-15 11:14:04 +0800

    feat: fix test case for router

--- src/components/ErrorBoundary/ErrorPage/ErrorPage.test.tsx
+++ src/components/ErrorBoundary/ErrorPage/ErrorPage.test.tsx
@@ -1,10 +1,18 @@
 import { render, screen } from "@testing-library/react";
+import { createMemoryHistory } from "history";
 import React from "react";
+import { Router } from "react-router-dom";
 import { ErrorPage } from "./ErrorPage";
 
 describe("ErrorPage", () => {
   it("should render correctly with the given title and description", () => {
-    render(<ErrorPage title="Error" description="Something went wrong!" />);
+    const history = createMemoryHistory();
+
+    render(
+      <Router history={history}>
+        <ErrorPage title="Error" description="Something went wrong!" />
+      </Router>
+    );
 
     expect(screen.getAllByText("Error")).toHaveLength(1);
     expect(screen.getAllByText("Something went wrong!")).toHaveLength(1);
