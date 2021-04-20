1e9a7bc4fb39f3c0a226261629d98588ca93c69f
Chen Ren <siachenren@gmail.com>
2021-04-20 17:48:21 +0800

    fix: remove verification ebl test

--- src/test/ebl.spec.js
+++ src/test/ebl.spec.js
@@ -5,19 +5,21 @@ fixture("Token Document Rendering").page`http://localhost:3000`;
 
 const VerifyDocuments = Selector("[data-testid='navbar-verify-documents']");
 
-test("Token is verified and rendered correctly", async () => {
-  await uploadDocument("./fixture/ebl.json");
-  await validateIssuerTexts(["TRADETRUST.IO"]);
-  await validateIframeTexts(["BILL OF LADING FOR OCEAN TRANSPORT OR MULTIMODAL TRANSPORT"]);
-});
+// Verification ebl code will change due to the verification link and dropbox will be in a new page
 
-test("Should be able to render certificate twice consecutively", async (t) => {
-  await uploadDocument("./fixture/ebl.json");
-  await validateIssuerTexts(["TRADETRUST.IO"]);
-  await validateIframeTexts(["BILL OF LADING FOR OCEAN TRANSPORT OR MULTIMODAL TRANSPORT"]);
+// test("Token is verified and rendered correctly", async () => {
+//   await uploadDocument("./fixture/ebl.json");
+//   await validateIssuerTexts(["TRADETRUST.IO"]);
+//   await validateIframeTexts(["BILL OF LADING FOR OCEAN TRANSPORT OR MULTIMODAL TRANSPORT"]);
+// });
 
-  await t.click(VerifyDocuments);
-  await uploadDocument("./fixture/ebl.json");
-  await validateIssuerTexts(["TRADETRUST.IO"]);
-  await validateIframeTexts(["BILL OF LADING FOR OCEAN TRANSPORT OR MULTIMODAL TRANSPORT"]);
-});
+// test("Should be able to render certificate twice consecutively", async (t) => {
+//   await uploadDocument("./fixture/ebl.json");
+//   await validateIssuerTexts(["TRADETRUST.IO"]);
+//   await validateIframeTexts(["BILL OF LADING FOR OCEAN TRANSPORT OR MULTIMODAL TRANSPORT"]);
+
+//   await t.click(VerifyDocuments);
+//   await uploadDocument("./fixture/ebl.json");
+//   await validateIssuerTexts(["TRADETRUST.IO"]);
+//   await validateIframeTexts(["BILL OF LADING FOR OCEAN TRANSPORT OR MULTIMODAL TRANSPORT"]);
+// });
