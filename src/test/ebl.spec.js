7d5b5ebeeedd21ac28e171d25f6ea6dcc26dfcfb
Chen Ren <siachenren@gmail.com>
2021-04-20 18:01:55 +0800

    fix : added skip test

--- src/test/ebl.spec.js
+++ src/test/ebl.spec.js
@@ -1,3 +1,4 @@
+/* eslint-disable jest/no-disabled-tests */
 import { uploadDocument, validateIframeTexts, validateIssuerTexts } from "./helper";
 import { Selector } from "testcafe";
 
@@ -7,19 +8,19 @@ const VerifyDocuments = Selector("[data-testid='navbar-verify-documents']");
 
 // Verification ebl code will change due to the verification link and dropbox will be in a new page
 
-// test("Token is verified and rendered correctly", async () => {
-//   await uploadDocument("./fixture/ebl.json");
-//   await validateIssuerTexts(["TRADETRUST.IO"]);
-//   await validateIframeTexts(["BILL OF LADING FOR OCEAN TRANSPORT OR MULTIMODAL TRANSPORT"]);
-// });
+test.skip("Token is verified and rendered correctly", async () => {
+  await uploadDocument("./fixture/ebl.json");
+  await validateIssuerTexts(["TRADETRUST.IO"]);
+  await validateIframeTexts(["BILL OF LADING FOR OCEAN TRANSPORT OR MULTIMODAL TRANSPORT"]);
+});
 
-// test("Should be able to render certificate twice consecutively", async (t) => {
-//   await uploadDocument("./fixture/ebl.json");
-//   await validateIssuerTexts(["TRADETRUST.IO"]);
-//   await validateIframeTexts(["BILL OF LADING FOR OCEAN TRANSPORT OR MULTIMODAL TRANSPORT"]);
+test.skip("Should be able to render certificate twice consecutively", async (t) => {
+  await uploadDocument("./fixture/ebl.json");
+  await validateIssuerTexts(["TRADETRUST.IO"]);
+  await validateIframeTexts(["BILL OF LADING FOR OCEAN TRANSPORT OR MULTIMODAL TRANSPORT"]);
 
-//   await t.click(VerifyDocuments);
-//   await uploadDocument("./fixture/ebl.json");
-//   await validateIssuerTexts(["TRADETRUST.IO"]);
-//   await validateIframeTexts(["BILL OF LADING FOR OCEAN TRANSPORT OR MULTIMODAL TRANSPORT"]);
-// });
+  await t.click(VerifyDocuments);
+  await uploadDocument("./fixture/ebl.json");
+  await validateIssuerTexts(["TRADETRUST.IO"]);
+  await validateIframeTexts(["BILL OF LADING FOR OCEAN TRANSPORT OR MULTIMODAL TRANSPORT"]);
+});
