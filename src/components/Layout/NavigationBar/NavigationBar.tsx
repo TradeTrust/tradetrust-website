41579169eef78654edbb6d2096eac0bacab3e52e
ChenRen <siachenren@gmail.com>
2021-05-03 13:53:46 +0800

    fix: change

--- src/components/Layout/NavigationBar/NavigationBar.tsx
+++ src/components/Layout/NavigationBar/NavigationBar.tsx
@@ -1,79 +1,11 @@
 import styled from "@emotion/styled";
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
 import { Settings } from "react-feather";
-import { NavigationItemType, NavigationItem, NavigationBar as NavBar } from "@govtechsg/tradetrust-ui-components";
+import { NavigationBar as NavBar } from "@govtechsg/tradetrust-ui-components";
+import { NavigationItem, NavigationItemType } from "./type";
+import { NavigationBarItem } from "./NavigationBarItem";
 
-const navItems: NavigationItem[] = [
-  {
-    schema: NavigationItemType.DropDownList,
-    id: "resources",
-    label: "Resources",
-    position: "left",
-    dropdownItems: [
-      {
-        id: "learn",
-        label: "Learn",
-        path: "/learn",
-      },
-      {
-        id: "faq",
-        label: "FAQ",
-        path: "/faq",
-      },
-    ],
-  },
-  {
-    schema: NavigationItemType.DropDownList,
-    id: "news_events",
-    label: "News & Events",
-    position: "left",
-    dropdownItems: [
-      {
-        id: "media",
-        label: "News",
-        path: "/media",
-      },
-      {
-        id: "event",
-        label: "Event",
-        path: "/event",
-      },
-    ],
-  },
-  {
-    schema: NavigationItemType.NavigationLink,
-    id: "contact",
-    label: "Contact",
-    path: "/contact",
-    position: "left",
-  },
-  {
-    schema: NavigationItemType.IconButton,
-    id: "settings",
-    label: "Settings",
-    path: "/settings",
-    icon: Settings,
-    position: "right",
-  },
-  {
-    schema: NavigationItemType.LabelButton,
-    id: "create-documents",
-    label: "Create Doc",
-    path: "https://creator.tradetrust.io/",
-    position: "right",
-    className: "create-btn",
-  },
-  {
-    schema: NavigationItemType.LabelButton,
-    id: "verify",
-    label: "Verify Doc",
-    path: "/verify",
-    position: "right",
-    className: "verify-btn",
-  },
-];
-
-const NavigationBarStyle = styled.nav`
+const NavigationBarStyle = styled.div`
   .create-btn {
     font-size: 16px;
     color: #3b8cc5;
@@ -95,10 +27,136 @@ const NavigationBarStyle = styled.nav`
   }
 `;
 
+const leftNavItems: NavigationItem[] = [
+  {
+    schema: NavigationItemType.DropDownList,
+    id: "resources",
+    label: "Resources",
+    path: "",
+    dropdownItems: [
+      {
+        id: "learn",
+        label: "Learn",
+        path: "/learn",
+      },
+      {
+        id: "faq",
+        label: "FAQ",
+        path: "/faq",
+      },
+    ],
+  },
+  {
+    schema: NavigationItemType.DropDownList,
+    id: "media_events",
+    label: "Media & Events",
+    path: "",
+    dropdownItems: [
+      {
+        id: "media",
+        label: "Media",
+        path: "/media",
+      },
+      {
+        id: "event",
+        label: "Event",
+        path: "/event",
+      },
+    ],
+  },
+  {
+    schema: NavigationItemType.NavigationLink,
+    id: "contact",
+    label: "Contact",
+    path: "/contact",
+  },
+];
+
+const leftMenu = (navigationItems: NavigationItem[], onClick: (isOn: boolean) => void) => {
+  return (
+    <div className="hidden lg:block md:ml-12">
+      <div className="flex h-full items-center">
+        {navigationItems.map((item, index) => {
+          return (
+            <div key={index} className="text-lg w-auto lg:ml-6">
+              <NavigationBarItem item={item} onClick={onClick} />
+            </div>
+          );
+          return "";
+        })}
+      </div>
+    </div>
+  );
+};
+
+const rightNavItems: NavigationItem[] = [
+  {
+    schema: NavigationItemType.IconButton,
+    id: "settings",
+    label: "Settings",
+    path: "settings",
+    icon: Settings,
+  },
+  {
+    schema: NavigationItemType.LabelButton,
+    id: "create-documents",
+    label: "Create Doc",
+    path: "https://creator.tradetrust.io/",
+    className: "create-btn",
+  },
+  {
+    schema: NavigationItemType.LabelButton,
+    id: "verify",
+    label: "Verify Doc",
+    path: "/verify",
+    className: "verify-btn",
+  },
+];
+
+const rightMenu = (navigationItems: NavigationItem[], onClick: (isOn: boolean) => void) => {
+  return (
+    <div className="hidden md:block md:absolute md:right-0 lg:relative lg:ml-auto">
+      <div className="flex h-full items-center">
+        {navigationItems.map((item, index) => {
+          return (
+            <div key={index} className="text-lg font-normal w-auto md:ml-3 lg:ml-6">
+              <NavigationBarItem item={item} onClick={onClick} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+const mobileMenu = (navigationItems: NavigationItem[], onClick: (isOn: boolean) => void) => {
+  return navigationItems.map((item, index) => {
+    if (item.id == "create-documents" || item.id == "verify" || item.id == "settings") {
+      return (
+        <div key={index} className="text-lg font-normal w-full py-4 md:hidden">
+          <NavigationBarItem item={item} onClick={onClick} />
+        </div>
+      );
+    }
+    return (
+      <div key={index} className="text-lg font-normal w-full py-4">
+        <NavigationBarItem item={item} onClick={onClick} />
+      </div>
+    );
+  });
+};
+
 export const NavigationBar: FunctionComponent<{}> = () => {
+  const [isOn, setIsOn] = useState(false);
   return (
     <NavigationBarStyle>
-      <NavBar navigationItems={navItems} />
+      <NavBar
+        leftMenuChildren={leftMenu(leftNavItems, setIsOn)}
+        rightMenuChildren={rightMenu(rightNavItems, setIsOn)}
+        mobileMenuChildren={mobileMenu(leftNavItems.concat(rightNavItems), setIsOn)}
+        onClick={setIsOn}
+        toggleNavBar={isOn}
+      />
     </NavigationBarStyle>
   );
 };
