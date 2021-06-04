c72f57e7725f432f334fd1b0712e84d5b9adca39
isaac <isaac.kps@gmail.com>
2021-06-04 15:38:15 +0800

    fix: revert back the code as reviewed

--- src/services/verify/fragments.tsx
+++ src/services/verify/fragments.tsx
@@ -1,21 +1,10 @@
-import { isValid, VerificationFragment, utils } from "@govtechsg/oa-verify";
+import { isValid, VerificationFragment } from "@govtechsg/oa-verify";
 
-export const getRevokeFragment = (fragments: VerificationFragment[]): VerificationFragment[] => {
-  const getOpenAttestationEthereumDocumentStoreRevokedFragment = utils.getFragmentByName(
-    "OpenAttestationEthereumDocumentStoreRevoked"
-  );
-  const revokeFragment = getOpenAttestationEthereumDocumentStoreRevokedFragment(fragments) as VerificationFragment;
-
-  return utils.isValidFragment(revokeFragment) ||
-    utils.isInvalidFragment(revokeFragment) ||
-    utils.isErrorFragment(revokeFragment)
-    ? [revokeFragment]
-    : [];
-};
-
-export const getNotRevokeFragment = (fragments: VerificationFragment[]): VerificationFragment[] => {
-  return fragments.filter((fragment) => fragment !== getRevokeFragment(fragments)[0]);
-};
+const revokeFragmentName = "OpenAttestationEthereumDocumentStoreRevoked";
+export const getNotRevokeFragment = (fragments: VerificationFragment[]): VerificationFragment[] =>
+  fragments.filter((status) => status.name !== revokeFragmentName && status.status !== "SKIPPED");
+export const getRevokeFragment = (fragments: VerificationFragment[]): VerificationFragment[] =>
+  fragments.filter((status) => status.name === revokeFragmentName && status.status !== "SKIPPED");
 
 interface interpretFragmentsReturnTypes {
   hashValid: boolean;
@@ -25,13 +14,11 @@ interface interpretFragmentsReturnTypes {
 }
 
 export const interpretFragments = (fragments: VerificationFragment[]): interpretFragmentsReturnTypes => {
-  const revokeFragments = getRevokeFragment(fragments);
   const notRevokeFragments = getNotRevokeFragment(fragments);
-
-  const hashValid = isValid(utils.getDocumentIntegrityFragments(fragments), ["DOCUMENT_INTEGRITY"]);
-  const issuedValid = isValid(utils.getDocumentStatusFragments(notRevokeFragments), ["DOCUMENT_STATUS"]);
-  const revokedValid =
-    revokeFragments.length === 0 || isValid(utils.getDocumentStatusFragments(revokeFragments), ["DOCUMENT_STATUS"]);
-  const identityValid = isValid(utils.getIssuerIdentityFragments(fragments), ["ISSUER_IDENTITY"]);
+  const revokeFragments = getRevokeFragment(fragments);
+  const hashValid = isValid(fragments, ["DOCUMENT_INTEGRITY"]);
+  const issuedValid = isValid(notRevokeFragments, ["DOCUMENT_STATUS"]);
+  const revokedValid = revokeFragments.length === 0 || isValid(revokeFragments, ["DOCUMENT_STATUS"]);
+  const identityValid = isValid(fragments, ["ISSUER_IDENTITY"]);
   return { hashValid, issuedValid, identityValid, revokedValid };
 };
