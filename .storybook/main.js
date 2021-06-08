57eee30e7fe9fae368a3bfd9f58f4b32fb0478ef
Sim Boon Long <simboonlong@gmail.com>
2021-06-08 14:01:37 +0800

    chore: update deps again before redesign (#361)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,25 @@
+const path = require("path");
+const toPath = (_path) => path.join(process.cwd(), _path);
+
 module.exports = {
-  stories: ["../src/**/*.stories.@(mdx|tsx)"],
+  stories: ["../src/**/*.stories.@(tsx)"],
   addons: ["@storybook/addon-essentials"],
+  typescript: {
+    reactDocgen: "none", // https://github.com/styleguidist/react-docgen-typescript/issues/356
+  },
+  webpackFinal: (config) => {
+    return {
+      ...config,
+      // https://github.com/storybookjs/storybook/issues/13277#issuecomment-751747964
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          "@emotion/core": toPath("node_modules/@emotion/react"),
+          "@emotion/styled": toPath("node_modules/@emotion/styled"),
+          "emotion-theming": toPath("node_modules/@emotion/react"),
+        },
+      },
+    };
+  },
 };
--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,16 @@
 module.exports = {
-  setupFiles: ["<rootDir>/jest.setup.js"],
-  testPathIgnorePatterns: ["<rootDir>/node_modules/"],
   collectCoverage: false,
+  collectCoverageFrom: ["src/**/*.{ts,tsx,js,jsx}", "scripts/**/*.{ts,tsx,js,jsx}", "!src/**/*.spec.{ts,tsx,js,jsx}"],
   coverageDirectory: "<rootDir>/.coverage/",
-  collectCoverageFrom: [
-    "src/**/*.{ts,tsx,js,jsx}",
-    "scripts/**/*.{ts,tsx,js,jsx}",
-    "!src/**/*.spec.{ts,tsx,js,jsx}"
-  ],
-  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)test.[jt]s?(x)"],
-  moduleFileExtensions: [
-      "ts",
-      "tsx",
-      "js",
-      "jsx",
-      "json",
-  ],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/_mocks_/fileMock.js",
-    "\\.(css|sass|scss)$": "identity-obj-proxy"
+    "\\.(css|sass|scss)$": "identity-obj-proxy",
   },
-  testResultsProcessor: "jest-sonar-reporter"
+  setupFiles: ["<rootDir>/jest.setup.js"],
+  testEnvironment: "jest-environment-jsdom",
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)test.[jt]s?(x)"],
+  testPathIgnorePatterns: ["<rootDir>/node_modules/"],
+  testResultsProcessor: "jest-sonar-reporter",
 };
