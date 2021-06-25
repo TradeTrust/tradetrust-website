522c9d93caded8431734d0d478132f179d7f5bd7
ChenRen <siachenren@gmail.com>
2021-06-25 11:09:08 +0800

    fix: port webinars to component

---
+++ src/components/Webinars/webinars.tsx
@@ -0,0 +1,32 @@
+import { getPaginatedPagesTotal, getPaginatedPosts, Pagination } from "@govtechsg/tradetrust-ui-components";
+import React from "react";
+import { FunctionComponent, useState } from "react";
+import { ResourceWebinar } from "../UI/ResourceWebinar";
+import { WebinarSingle } from "./types";
+
+export const webinarPerPage = 6;
+
+export const PaginatedWebinar: FunctionComponent<{ webinars: WebinarSingle[] }> = ({ webinars }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const paginatedPosts = getPaginatedPosts({ posts: webinars, postsPerPage: webinarPerPage, currentPage });
+  const totalNoOfPages = getPaginatedPagesTotal({ posts: webinars, postsPerPage: webinarPerPage });
+
+  return (
+    <>
+      {paginatedPosts.map((webinar, index) => (
+        <ResourceWebinar
+          title={webinar.title}
+          description={webinar.description}
+          resource={webinar.resource}
+          key={index}
+        />
+      ))}
+
+      {totalNoOfPages > 1 && (
+        <div className="w-min mx-auto my-14">
+          <Pagination totalNoOfPages={totalNoOfPages} currentPage={currentPage} setCurrentPage={setCurrentPage} />
+        </div>
+      )}
+    </>
+  );
+};
