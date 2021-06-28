7f0aa0375c00be2b1bb3c6ff30c0b17e674c6391
Chen Ren <siachenren@gmail.com>
2021-06-28 15:27:54 +0800

    feat: redesign learn page

--- src/components/UI/ResourceLink/ResourceLink.tsx
+++ src/components/UI/ResourceLink/ResourceLink.tsx
@@ -9,14 +9,14 @@ export interface ResourceLinkProps {
 
 export const ResourceLink: FunctionComponent<ResourceLinkProps> = ({ title, resources, icon }) => {
   return (
-    <div className="bg-white shadow-md mb-4 pt-2 px-3 pb-3">
+    <div className="bg-white shadow-md rounded-lg mb-4 py-2 px-4">
       <div className="flex">
         <div className="flex-grow">
-          <div className="text-gray-700 text-xl font-medium">{title}</div>
+          <h4 className="text-gray-700">{title}</h4>
           {resources.map((resource, index) => (
-            <div className="pt-2 text-blue" key={index}>
+            <div className="text-blue" key={index}>
               <a
-                className="text-base font-medium flex items-end mb-2"
+                className="text-base font-bold flex items-end mb-2"
                 href={resource.url}
                 target="_blank"
                 rel="noopener noreferrer"
@@ -25,7 +25,7 @@ export const ResourceLink: FunctionComponent<ResourceLinkProps> = ({ title, reso
                 {resource.title}
               </a>
               {resource.date && (
-                <div className="text-gray-500 text-base font-medium border-b border-solid border-gray-300 pb-4 mb-1">
+                <div className="text-gray-500 text-base font-medium border-b border-solid border-gray-300">
                   {resource.date}
                 </div>
               )}
