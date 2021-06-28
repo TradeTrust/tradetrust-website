7fe6d5be068e7a72e14771f0bb3422336704ed6a
Sim Boon Long <simboonlong@gmail.com>
2021-06-28 13:26:16 +0800

    chore: remove slowMo

--- integration/local/metamask-init.mjs
+++ integration/local/metamask-init.mjs
@@ -8,7 +8,6 @@ export const metamaskInit = async () => {
       executablePath: process.env.PUPPETEER_EXEC_PATH,
       headless: false, // must be false, so can test with metamask extension
       defaultViewport: null,
-      slowMo: 10, // to see what's gg on
     });
     const metamask = await dappeteer.getMetamask(browser, {
       seed: "indicate swing place chair flight used hammer soon photo region volume shuffle",
