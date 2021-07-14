3f86624e5713339da90b58ba2d6db789d83368c6
Kyle Huang Junyuan <kyle@hjunyuan.com>
2021-07-14 19:05:14 +0800

    fix: update qr processor tests

--- src/services/qrProcessor/qrProcessor.test.js
+++ src/services/qrProcessor/qrProcessor.test.js
@@ -1,39 +1,64 @@
 import { decodeQrCode, processQrCode } from "./index";
 
 const encodeQrCode = (payload) => `https://action.openattestation.com?q=${encodeURIComponent(JSON.stringify(payload))}`;
+const encodeQrCodeWithAnchor = (payload, anchor) =>
+  `https://action.openattestation.com?q=${encodeURIComponent(JSON.stringify(payload))}#${encodeURIComponent(
+    JSON.stringify(anchor)
+  )}`;
 
 describe("decodeQrCode", () => {
   it("decodes an action correctly", () => {
-    const encodedQrCode =
-      "https://action.openattestation.com?q=%7B%22uri%22%3A%22https%3A%2F%2Fsample.domain%2Fdocument%2Fid%3Fq%3Dabc%23123%22%7D";
-
-    const action = decodeQrCode(encodedQrCode);
-    expect(action).toStrictEqual({
+    const sampleAction = {
       uri: "https://sample.domain/document/id?q=abc#123",
-    });
+    };
+
+    const { action, anchor } = decodeQrCode(encodeQrCode(sampleAction));
+    expect(action).toStrictEqual(action);
+    expect(anchor).toStrictEqual({});
   });
 
-  it("throws when qr code is malformed", () => {
+  it("decodes an action and anchor correctly", () => {
+    const sampleAction = {
+      uri: "https://sample.domain/document/id?q=abc#123",
+    };
+    const sampleAnchor = { key: "secret" };
+
+    const { action, anchor } = decodeQrCode(encodeQrCodeWithAnchor(sampleAction, sampleAnchor));
+    expect(action).toStrictEqual(action);
+    expect(anchor).toStrictEqual(anchor);
+  });
+
+  it("throws error when qr code is malformed", () => {
     const encodedQrCode = "http://%7B%22uri%22%3A%22https%3A%2F%2Fsample.domain%2Fdocument%2Fid%3Fq%3Dabc%23123%22%7D";
-    expect(() => decodeQrCode(encodedQrCode)).toThrow("not formatted");
+    expect(() => decodeQrCode(encodedQrCode)).toThrow(`Invalid URL: ${encodedQrCode}`);
   });
 });
 
 describe("processQrCode", () => {
-  it("throws error when uri is not document", async () => {
+  it("throws error when action type is not supported", async () => {
     const document = { name: "foo" };
     const type = "MANY_DOCUMENT";
-    // const { key } = await encryptString(JSON.stringify(document));
     const actionUri = { payload: document, type };
+
     await expect(() => processQrCode(encodeQrCode(actionUri))).rejects.toThrow(
       `The type ${type} provided from the action is not supported`
     );
   });
 
-  it("fetches calls get with the right parameter when a QR code is scanned", async () => {
+  it("extracts the correct payload from a QR code", async () => {
     const document = { name: "foo" };
-    const actionUri = { payload: document, type: "DOCUMENT" };
-    const results = await processQrCode(encodeQrCode(actionUri));
-    expect(results).toStrictEqual(document);
+    const sampleAction = { payload: document, type: "DOCUMENT" };
+
+    const results = await processQrCode(encodeQrCode(sampleAction));
+    expect(results).toStrictEqual({ payload: document, anchor: {} });
+  });
+
+  it("extracts the correct payload and anchor from a QR code", async () => {
+    const document = { name: "foo" };
+    const sampleAction = { payload: document, type: "DOCUMENT" };
+    const sampleAnchor = { key: "secret" };
+
+    const results = await processQrCode(`${encodeQrCodeWithAnchor(sampleAction, sampleAnchor)}`);
+    expect(results).toStrictEqual({ payload: document, anchor: sampleAnchor });
   });
 });
