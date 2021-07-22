ad4b164f225d6519c940f1c1324c9a0e7d15dfcb
ryansee <ryansee.sw@outlook.com>
2021-07-22 12:17:15 +0800

    feat: redesign settings page 2

--- src/pages/settings/settingsAddressBook.tsx
+++ src/pages/settings/settingsAddressBook.tsx
@@ -16,8 +16,10 @@ export const SettingsAddressBookPage: FunctionComponent = () => (
       <Link to="/settings">
         <BackArrow />
       </Link>
-      <h3>Settings: Address Book</h3>
-      <p className="text-gray-500">Please select an address book to view. </p>
+      <div className="flex-1 flex-col">
+        <h3 className="font-ubuntu text-4xl text-cloud-900">Address Book</h3>
+        <p className="mb-5 text-cloud-900">Please select an address book to view. </p>
+      </div>
       <AddressBook className="py-6" network="ropsten" />
     </div>
   </>
