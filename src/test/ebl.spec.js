9b20f26281fa2daecdf1e5e7ff54b0ffd45d7a89
Sim Boon Long <simboonlong@gmail.com>
2021-08-02 16:12:23 +0800

    refactor: address resolver e2e (#403)

--- src/test/ebl.spec.js
+++ src/test/ebl.spec.js
@@ -1,24 +1,21 @@
-import { uploadDocument, validateIframeTexts, validateIssuerTexts, location } from "./helper";
-import { Selector } from "testcafe";
+import { uploadDocument, validateIframeTexts, validateIssuerTexts, location, navigateToVerify } from "./helper";
 
 fixture("Token Document Rendering").page`${location}`;
 
-const VerifyDocuments = Selector("[data-testid='navbar-verify-documents']");
-
 test("Token is verified and rendered correctly", async () => {
+  await navigateToVerify();
   await uploadDocument("./fixture/ebl.json");
   await validateIssuerTexts(["TRADETRUST.IO"]);
   await validateIframeTexts(["BILL OF LADING FOR OCEAN TRANSPORT OR MULTIMODAL TRANSPORT"]);
 });
 
-// Skipped test due to changing verify to different page
-// eslint-disable-next-line jest/no-disabled-tests
-test.skip("Should be able to render certificate twice consecutively", async (t) => {
+test("Should be able to render certificate twice consecutively", async (t) => {
+  await navigateToVerify();
   await uploadDocument("./fixture/ebl.json");
   await validateIssuerTexts(["TRADETRUST.IO"]);
   await validateIframeTexts(["BILL OF LADING FOR OCEAN TRANSPORT OR MULTIMODAL TRANSPORT"]);
 
-  await t.click(VerifyDocuments);
+  await navigateToVerify();
   await uploadDocument("./fixture/ebl.json");
   await validateIssuerTexts(["TRADETRUST.IO"]);
   await validateIframeTexts(["BILL OF LADING FOR OCEAN TRANSPORT OR MULTIMODAL TRANSPORT"]);
