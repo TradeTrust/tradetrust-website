a11cd890dcc27561d06bb4f7b75afc85652cf796
ChenRen <siachenren@gmail.com>
2021-08-10 18:30:04 +0800

    fix: remove forward slash

--- public/service-worker.js
+++ public/service-worker.js
@@ -29,7 +29,7 @@ self.addEventListener("install", function (event) {
       caches.open(CACHE_NAME).then(function (cache) {
         // Get the assets manifest so we can see what our js file is named
         // This is because webpack hashes it
-        fetch("/manifest.json")
+        fetch("manifest.json")
           .then((response) => {
             response.json();
           })
