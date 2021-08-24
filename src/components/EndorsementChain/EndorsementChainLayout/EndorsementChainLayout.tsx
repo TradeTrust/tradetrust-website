1bcc651b80fd0108c417bb7ade36a51ed7bdf998
ryansee <ryansee.sw@outlook.com>
2021-08-25 01:37:46 +0800

    fix: endorsement chain dotted path

--- src/components/EndorsementChain/EndorsementChainLayout/EndorsementChainLayout.tsx
+++ src/components/EndorsementChain/EndorsementChainLayout/EndorsementChainLayout.tsx
@@ -7,6 +7,28 @@ import { TitleEscrowEvent, TradeTrustErc721Event } from "../../../types";
 import { TooltipIcon } from "../../UI/SvgIcon";
 import { EndorsementChainError } from "./EndorsementChainError";
 import { EndorsementChainLoading } from "./EndorsementChainLoading";
+import styled from "@emotion/styled";
+
+const EndorsementChainDataStyle = styled.div`
+  & > *:first-of-type {
+    .dot-path {
+      bottom: 0;
+      height: 50%;
+    }
+  }
+
+  & > *:last-of-type {
+    // for desktop scren
+    .dot-path {
+      height: 50%;
+    }
+
+    // for mobile screen
+    .path {
+      height: 0;
+    }
+  }
+`;
 
 type EndorsementChain = (TradeTrustErc721Event | TitleEscrowEvent)[];
 interface EndorsementChainLayout {
@@ -59,72 +81,6 @@ interface DetailsEntityProps {
   address: string;
   documentOwner: string;
 }
-
-const DetailsEntity: React.FunctionComponent<DetailsEntityProps> = ({ title, address, documentOwner }) => {
-  const tooltipContent = (
-    <div className="relative flex flex-col">
-      <div className="text-white font-bold text-base">Title Escrow:</div>
-      <div className="text-white text-base">{documentOwner}</div>
-    </div>
-  );
-
-  return (
-    <div className="w-full lg:w-1/3">
-      <div className="flex flex-nowrap pr-8">
-        <div className="lg:hidden" style={{ width: "40px" }}>
-          <div className="relative h-full">
-            <div className="border-l border-dashed border-cerulean absolute top-0 left-1/2 h-full" />
-          </div>
-        </div>
-        <div className="pb-4 lg:pb-0">
-          <h5 className="text-gray-400 mr-2 lg:hidden">{title}</h5>
-          <div className="flex flex-wrap items-center">
-            <div className="w-auto">
-              <AddressResolvedName address={address} />
-            </div>
-            <div className="w-auto">
-              <TooltipIcon className="h-5 w-5 block" content={tooltipContent} placement="top">
-                <Info />
-              </TooltipIcon>
-            </div>
-          </div>
-          <h6 className="text-cerulean break-all">{address}</h6>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-const EndorsementChainData: React.FunctionComponent<any> = ({ data }) => {
-  return (
-    <div className="flex flex-wrap items-center">
-      <div className="w-full lg:w-1/3">
-        <div className="flex flex-nowrap">
-          <div className="lg:order-2" style={{ width: "40px" }}>
-            <div className="relative h-full">
-              <div className="border-l border-dashed border-cerulean absolute top-0 left-1/2 h-full" />
-              <div className="flex justify-center items-center h-full">
-                <div className="rounded-full bg-cerulean h-3 w-3 mx-auto" />
-              </div>
-            </div>
-          </div>
-          <div className="lg:ml-auto lg:order-1">
-            <div className="lg:text-right py-6 lg:py-4">
-              <h4 className="text-cloud-900">{data.action}</h4>
-              {data.timestamp && (
-                <h6 className="text-gray-400">{format(new Date(data.timestamp ?? 0), "do MMM yyyy, hh:mm aa")}</h6>
-              )}
-            </div>
-          </div>
-        </div>
-      </div>
-      {data.beneficiary && (
-        <DetailsEntity title="Owner" address={data.beneficiary} documentOwner={data.documentOwner} />
-      )}
-      {data.holder && <DetailsEntity title="Holder" address={data.holder} documentOwner={data.documentOwner} />}
-    </div>
-  );
-};
 
 const getHistoryChain = (endorsementChain?: EndorsementChain) => {
   const historyChain: HistoryChain[] = [
@@ -227,6 +183,72 @@ const getHistoryChain = (endorsementChain?: EndorsementChain) => {
   return historyChain;
 };
 
+const DetailsEntity: React.FunctionComponent<DetailsEntityProps> = ({ title, address, documentOwner }) => {
+  const tooltipContent = (
+    <div className="relative flex flex-col">
+      <div className="text-white font-bold text-base">Title Escrow:</div>
+      <div className="text-white text-base">{documentOwner}</div>
+    </div>
+  );
+
+  return (
+    <div className="w-full lg:w-1/3">
+      <div className="flex flex-nowrap pr-8">
+        <div className="flex-shrink-0 lg:hidden" style={{ width: "40px" }}>
+          <div className="relative h-full">
+            <div className="path border-l border-dashed border-cerulean absolute top-0 left-1/2 h-full" />
+          </div>
+        </div>
+        <div className="pb-4 lg:pb-0">
+          <h5 className="text-gray-400 mr-2 lg:hidden">{title}</h5>
+          <div className="flex flex-wrap items-center">
+            <div className="w-auto">
+              <AddressResolvedName address={address} />
+            </div>
+            <div className="w-auto">
+              <TooltipIcon className="h-5 w-5 block" content={tooltipContent} placement="top">
+                <Info />
+              </TooltipIcon>
+            </div>
+          </div>
+          <h6 className="text-cerulean break-all">{address}</h6>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const EndorsementChainData: React.FunctionComponent<any> = ({ data }) => {
+  return (
+    <div className="flex flex-wrap items-center">
+      <div className="w-full lg:w-1/3">
+        <div className="flex flex-nowrap">
+          <div className="flex-shrink-0 lg:order-2" style={{ width: "40px" }}>
+            <div className="relative h-full">
+              <div className="dot-path border-l border-dashed border-cerulean absolute left-1/2 h-full" />
+              <div className="flex justify-center items-center h-full">
+                <div className="rounded-full bg-cerulean h-3 w-3 mx-auto" />
+              </div>
+            </div>
+          </div>
+          <div className="lg:ml-auto lg:order-1">
+            <div className="lg:text-right py-6 lg:py-4">
+              <h4 className="text-cloud-900">{data.action}</h4>
+              {data.timestamp && (
+                <h6 className="text-gray-400">{format(new Date(data.timestamp ?? 0), "do MMM yyyy, hh:mm aa")}</h6>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+      {data.beneficiary && (
+        <DetailsEntity title="Owner" address={data.beneficiary} documentOwner={data.documentOwner} />
+      )}
+      {data.holder && <DetailsEntity title="Holder" address={data.holder} documentOwner={data.documentOwner} />}
+    </div>
+  );
+};
+
 export const EndorsementChainLayout: FunctionComponent<EndorsementChainLayout> = ({
   endorsementChain,
   setShowEndorsementChain,
@@ -237,28 +259,37 @@ export const EndorsementChainLayout: FunctionComponent<EndorsementChainLayout> =
 
   return (
     <div className="">
-      <div className="cursor-pointer" onClick={() => setShowEndorsementChain(false)} data-testid="back-button">
-        <BackArrow />
-      </div>
-      <div data-testid="endorsement-chain-title">
-        <h3>Endorsement Chain</h3>
-      </div>
-      <div className="bg-white rounded-xl shadow-xl px-3 py-8 lg:px-8">
-        <div className="hidden lg:block mb-8">
-          <div className="flex text-gray-400">
-            <h5 className="w-1/3">Action/Date</h5>
-            <h5 className="w-1/3">Owner</h5>
-            <h5 className="w-1/3">Holder</h5>
-          </div>
-          <div className="border-t" />
+      <div className="">
+        <div className="cursor-pointer" onClick={() => setShowEndorsementChain(false)} data-testid="back-button">
+          <BackArrow />
         </div>
+        <div data-testid="endorsement-chain-title">
+          <h3>Endorsement Chain</h3>
+        </div>
+        <div className="bg-white rounded-xl shadow-xl px-3 py-8 lg:px-8">
+          <div className="hidden lg:block mb-8">
+            <div className="flex text-gray-400">
+              <h5 className="w-1/3">Action/Date</h5>
+              <h5 className="w-1/3">Owner</h5>
+              <h5 className="w-1/3">Holder</h5>
+            </div>
+            <div className="border-t" />
+          </div>
 
-        {pending && !endorsementChain && !error && <EndorsementChainLoading />}
-        {!pending &&
-          endorsementChain &&
-          !error &&
-          historyChain.map((item, key) => <EndorsementChainData data={item} key={key} />)}
-        {!pending && !endorsementChain && error && <EndorsementChainError error={error} />}
+          {pending && !endorsementChain && !error && <EndorsementChainLoading />}
+          {/* {!pending &&
+            endorsementChain &&
+            !error &&
+            historyChain.map((item, key) => <EndorsementChainData data={item} key={key} />)} */}
+          {!pending && endorsementChain && !error && (
+            <EndorsementChainDataStyle>
+              {historyChain.map((item, key) => (
+                <EndorsementChainData data={item} key={key} />
+              ))}
+            </EndorsementChainDataStyle>
+          )}
+          {!pending && !endorsementChain && error && <EndorsementChainError error={error} />}
+        </div>
       </div>
     </div>
   );
