630648c83f2a795b531c5905ce8f6c2c945f4e8b
ryansee <ryansee.sw@outlook.com>
2021-09-14 14:00:25 +0800

    fix: removed commented codes

--- src/components/HomePageContent/HowItWorksSection/index.tsx
+++ src/components/HomePageContent/HowItWorksSection/index.tsx
@@ -76,13 +76,6 @@ export const HowItWorksSection: FunctionComponent = () => {
               Ready to learn how TradeTrust can benefit your business?
             </h3>
           </GetInTouchBackground>
-          {/* <Link
-            to="/contact"
-            className="px-4 py-2 mx-auto -mt-4 rounded-xl text-white bg-tangerine hover:bg-tangerine-600 hover:text-gray-200"
-            data-testid="get-in-touch"
-          >
-            <h3 className="font-normal text-2xl">Get in Touch Now</h3>
-          </Link> */}
           <Link
             to="/demo"
             className="px-4 py-2 mx-auto -mt-4 rounded-xl text-white bg-tangerine hover:bg-tangerine-600 hover:text-gray-200"
