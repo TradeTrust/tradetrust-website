f290c611d63e4a7a4dac8f792c94db606c7587e6
isaac <isaac.kps@gmail.com>
2021-10-21 14:48:51 +0800

    feat: update page 404

--- src/pages/pageNotFound.tsx
+++ src/pages/pageNotFound.tsx
@@ -1,16 +1,18 @@
-import { ErrorPage } from "@govtechsg/tradetrust-ui-components";
+import { Page404 } from "@govtechsg/tradetrust-ui-components";
 import React, { FunctionComponent } from "react";
 import { Link } from "react-router-dom";
 
 export const PageNotFound: FunctionComponent = () => {
   return (
-    <ErrorPage title="404" description="Oops, page not found">
-      <Link
-        className="mt-4 inline-block px-8 py-4 bg-cerulean-50 hover:bg-tangerine-600 text-white hover:text-white border-none rounded-full font-semibold uppercase no-underline transition duration-300 ease-out text-sm"
-        to="/"
-      >
-        Go back to home
-      </Link>
-    </ErrorPage>
+    <Page404>
+      <h3 className="font-ubuntu font-normal w-60 sm:w-80 my-2 sm:my-4 text-lg sm:text-2xl">
+        Go to
+        <Link className="text-cerulean-200" to="/">
+          {" "}
+          Homepage
+        </Link>
+        ?
+      </h3>
+    </Page404>
   );
 };
