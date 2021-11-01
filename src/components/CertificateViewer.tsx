5747479bd6ea67baa57a436cb08a0337512b68cb
waynewee <34233605+waynewee@users.noreply.github.com>
2021-11-01 10:24:54 +0800

    feat: add banner to magic demo (#471)

--- src/components/CertificateViewer.tsx
+++ src/components/CertificateViewer.tsx
@@ -104,10 +104,14 @@ export const CertificateViewer: FunctionComponent<CertificateViewerProps> = ({ i
     <>
       <div className="no-print">
         {!isTransferableDocument && <DocumentStatus isMagicDemo={isMagicDemo} />}
-        {isSampleDocument && (
+        {(isSampleDocument || isMagicDemo) && (
           <Banner
             className="mt-8"
-            title="Want to try creating a verifiable document? You will be surprised how easy it is."
+            title={
+              isMagicDemo
+                ? "Ready to learn how TradeTrust can benefit your business?"
+                : "Want to try creating a verifiable document? You will be surprised how easy it is."
+            }
           />
         )}
         <ObfuscatedMessage document={document} />
