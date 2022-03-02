f17dae8ecb81de87dde1a4bfd996d9f7bf5b45d8
isaac <isaac.kps@gmail.com>
2022-03-02 17:10:36 +0800

    feat: added auto network switching for document with network field

---
+++ src/utils/shared.test.ts
@@ -0,0 +1,71 @@
+import { getChainId, WrappedOrSignedOpenAttestationDocument } from "./shared";
+import ebl from "../test/fixture/ebl.json";
+import invoice from "../test/fixture/v3/invoice-ropsten.json";
+import v2DID from "../test/fixture/sample-dns-did-verified.json";
+import v3DID from "../test/fixture/v3/dns-did-signed.json";
+
+describe("getChainId", () => {
+  it("should return the correct chainId for v2 document", () => {
+    const document = {
+      ...ebl,
+      data: { ...ebl.data, network: { chain: "ETH", chainId: "3" } },
+    } as unknown as WrappedOrSignedOpenAttestationDocument;
+    expect(getChainId(document)).toStrictEqual(3);
+  });
+
+  it("should return 'undefined' when there is not network for v2 document", () => {
+    expect(getChainId(ebl as unknown as WrappedOrSignedOpenAttestationDocument)).toStrictEqual(undefined);
+  });
+
+  it("should throw an error when the chainId is not in the network object for v2 document", () => {
+    const document = {
+      ...ebl,
+      data: { ...ebl.data, network: { chain: "ETH" } },
+    } as unknown as WrappedOrSignedOpenAttestationDocument;
+    expect(() => getChainId(document)).toThrow("Invalid Document, please use a valid document.");
+  });
+
+  it("should throw an error when the chainId is not in the list of networks for v2 document", () => {
+    const document = {
+      ...ebl,
+      data: { ...ebl.data, network: { chain: "ETH", chainId: "8" } },
+    } as unknown as WrappedOrSignedOpenAttestationDocument;
+    expect(() => getChainId(document)).toThrow("Invalid Document, please use a valid document.");
+  });
+
+  it("should return 'undefined' when a V2 did document is being dropped", () => {
+    expect(getChainId(v2DID as unknown as WrappedOrSignedOpenAttestationDocument)).toStrictEqual(undefined);
+  });
+
+  it("should return the correct chainId for v3 document", () => {
+    const document = {
+      ...invoice,
+      network: { chain: "ETH", chainId: "3" },
+    } as unknown as WrappedOrSignedOpenAttestationDocument;
+    expect(getChainId(document)).toStrictEqual(3);
+  });
+
+  it("should return 'undefined' when there is not network for v3 document", () => {
+    expect(getChainId(invoice as unknown as WrappedOrSignedOpenAttestationDocument)).toStrictEqual(undefined);
+  });
+
+  it("should throw an error when the chainId is not in the network object for v3 document", () => {
+    const document = {
+      ...invoice,
+      network: { chain: "ETH" },
+    } as unknown as WrappedOrSignedOpenAttestationDocument;
+    expect(() => getChainId(document)).toThrow("Invalid Document, please use a valid document.");
+  });
+
+  it("should throw an error when the chainId is not in the list of networks for v3 document", () => {
+    const document = {
+      ...invoice,
+      network: { chain: "ETH", chainId: "8" },
+    } as unknown as WrappedOrSignedOpenAttestationDocument;
+    expect(() => getChainId(document)).toThrow("Invalid Document, please use a valid document.");
+  });
+
+  it("should return 'undefined' when a V3 did document is being dropped", () => {
+    expect(getChainId(v3DID as unknown as WrappedOrSignedOpenAttestationDocument)).toStrictEqual(undefined);
+  });
+});
--- src/utils/shared.ts
+++ src/utils/shared.ts
@@ -1,4 +1,7 @@
 import { getData, utils, v2, v3, OpenAttestationDocument, WrappedDocument } from "@govtechsg/open-attestation";
+import { ChainId } from "../constants/chain-info";
+import { IS_DEVELOPMENT } from "../config";
+import { MAIN_NETWORKS, TEST_NETWORKS } from "../config/chain-config";
 
 export type WrappedOrSignedOpenAttestationDocument = WrappedDocument<OpenAttestationDocument>;
 // note that the return type for getting attachments will normalise the structure into v2.Attachment
@@ -35,4 +38,45 @@ export const getAttachments = (rawDocument: WrappedOrSignedOpenAttestationDocume
 export const getTokenRegistryAddress = (document: WrappedOrSignedOpenAttestationDocument): string | undefined => {
   const issuerAddress = utils.getIssuerAddress(document);
   return issuerAddress instanceof Array ? issuerAddress[0] : issuerAddress;
+};
+
+export const getChainId = (rawDocument: WrappedOrSignedOpenAttestationDocument): ChainId | undefined => {
+  const warn = () =>
+    console.warn(
+      "You are using an older version of Open-Attestation Document, to use the auto network feature, please use an updated version. Otherwise, please make sure that you select the correct network."
+    );
+
+  const throwError = () => {
+    throw new Error("Invalid Document, please use a valid document.");
+  };
+
+  const networks = IS_DEVELOPMENT ? [...TEST_NETWORKS] : [...MAIN_NETWORKS];
+
+  const processChainId = (document: OpenAttestationDocument): number | undefined => {
+    if (document.network) {
+      // Check for current bc, "ETH", and chainId, if need cater for other bc and network, update this accordingly. (V2)
+      if (document.network.chain === "ETH" && document.network.chainId) {
+        const chainIdNumber = parseInt(document.network.chainId);
+        const isChainIdInListedNetwork = networks.includes(chainIdNumber);
+        if (!chainIdNumber || !isChainIdInListedNetwork) throwError();
+        return chainIdNumber;
+      }
+      throwError();
+    }
+    warn();
+    return undefined;
+  };
+
+  if (utils.isWrappedV2Document(rawDocument)) {
+    const documentData = getData(rawDocument);
+    // Check for DID, ignore chainId when its DID
+    const identityProofType = documentData.issuers[0].identityProof?.type;
+    if (identityProofType === "DNS-DID" || identityProofType === "DID") return undefined;
+    return processChainId(documentData);
+  } else if (utils.isWrappedV3Document(rawDocument)) {
+    // Check for DID, ignore chainId when its DID
+    const identityProofType = rawDocument.openAttestationMetadata.identityProof.type;
+    if (identityProofType === "DNS-DID" || identityProofType === "DID") return undefined;
+    return processChainId(rawDocument);
+  }
 };
