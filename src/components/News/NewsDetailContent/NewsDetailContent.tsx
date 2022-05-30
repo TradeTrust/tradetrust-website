19b3305f8bfa32fdaad9812fadbcbb1ca402d810
isaackps <isaac.kps@gmail.com>
2022-05-30 14:54:52 +0800

    fix: color for event and news page (#574)
    * fix: color for event and news page
    
    * fix: update ui repo
    
    * fix: update ui package

--- src/components/News/NewsDetailContent/NewsDetailContent.tsx
+++ src/components/News/NewsDetailContent/NewsDetailContent.tsx
@@ -6,10 +6,10 @@ import { format } from "date-fns";
 
 export const NewsDetailContent: FunctionComponent<{ detail: NewsSingle }> = ({ detail }) => {
   return (
-    <div className="bg-white text-cloud-800 shadow-lg rounded-lg p-8">
-      <TagBorderedSm className="mb-4">{detail.type}</TagBorderedSm>
+    <div className="bg-white text-cloud-800 drop-shadow-xl rounded-xl p-8">
+      <TagBorderedSm className="mb-4 border-cloud-300 text-cloud-800">{detail.type}</TagBorderedSm>
       <h3 className="mb-4 leading-8">{detail.attributes.title}</h3>
-      <p className="text-sm text-cloud-400 mb-4">{format(new Date(detail.attributes.date), "d MMM yyyy")}</p>
+      <p className="text-sm text-cloud-300 mb-4">{format(new Date(detail.attributes.date), "d MMM yyyy")}</p>
       <ReactMarkdown className="wysiwyg">{detail.body}</ReactMarkdown>
     </div>
   );
