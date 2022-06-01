ab67a12f8fa0febed6403d7b2c2858d9b134ee32
superical <superical@users.noreply.github.com>
2022-06-01 18:59:36 +0800

    test: connect wallet and manage asset button in e2e (#577)

--- tests/e2e/specs/transfer-holder.spec.js
+++ tests/e2e/specs/transfer-holder.spec.js
@@ -22,8 +22,7 @@ describe(
       cy.get("input[type=file]").attachFile("ebl-transfer-holder.json");
       cy.get("[data-testid='asset-title-owner']").should("be.visible");
       cy.get("[data-testid='asset-title-holder']").should("be.visible");
-      cy.get("[data-testid='connectToWallet']").click();
-      cy.get("[data-testid='manageAssetDropdown']").click();
+      cy.clickConnectAndManageAssetButton();
       cy.get("[data-testid='transferHolderDropdown']").click();
       cy.get("[data-testid='editable-input-holder']").type(ACCOUNT_2);
       cy.get("[data-testid='transferBtn']").click();
--- tests/e2e/support/commands.js
+++ tests/e2e/support/commands.js
@@ -1,1 +1,15 @@
 import "cypress-file-upload";
+
+Cypress.Commands.add("clickConnectAndManageAssetButton", (acceptMetamask = false) => {
+  cy.get("#title-transfer-panel").then(($transferPanel) => {
+    cy.wait(2000);
+    if ($transferPanel.find("[data-testid='connectToWallet']").length) {
+      cy.get("[data-testid='connectToWallet']").click();
+      if (acceptMetamask) {
+        cy.acceptMetamaskAccess(true).should("be.true");
+        cy.wait(2000);
+      }
+    }
+  });
+  cy.get("[data-testid='manageAssetDropdown']").click();
+});
