56b89d78b905c76bda6daf71566f3ee5788c3312
isaac <isaac.kps@gmail.com>
2022-06-23 15:43:27 +0800

    chore: combine surrender test cases

--- tests/e2e/specs/surrender-document.spec.js
+++ tests/e2e/specs/surrender-document.spec.js
@@ -17,7 +17,7 @@ describe(
       });
     });
 
-    context("Surrender", () => {
+    context("Surrender Reject", () => {
       it("should go to verify page, upload a file, connect to wallet and surrender a document successfully", () => {
         cy.visit("/verify");
         cy.get("input[type=file]").attachFile("ebl-surrender.json");
@@ -30,9 +30,7 @@ describe(
         cy.get("#surrender-sign").should("have.text", "Surrendered To Issuer");
         cy.get("[data-testid='overlay-title']").should("have.text", "Surrender Document Success");
       });
-    });
 
-    context("Reject Surrender", () => {
       it("should go to verify page, upload a file, connect to wallet and reject the surrendered document successfully", () => {
         cy.visit("/verify");
         cy.get("input[type=file]").attachFile("ebl-surrender.json");
@@ -43,6 +41,32 @@ describe(
         cy.get("[data-testid='confirmActionBtn']").click();
         cy.confirmMetamaskTransaction();
         cy.get("[data-testid='overlay-title']").should("have.text", "Surrender Rejected");
+      });
+    });
+
+    context("Surrender Accept", () => {
+      it("should go to verify page, upload a file, connect to wallet and surrender a document successfully", () => {
+        cy.visit("/verify");
+        cy.get("input[type=file]").attachFile("ebl-surrender.json");
+        cy.get("[data-testid='asset-title-owner']").should("be.visible");
+        cy.get("[data-testid='asset-title-holder']").should("be.visible");
+        cy.checkAcceptMetamaskAccess();
+        cy.get("[data-testid='surrenderDropdown']").click();
+        cy.get("[data-testid='surrenderBtn']").click();
+        cy.confirmMetamaskTransaction();
+        cy.get("#surrender-sign").should("have.text", "Surrendered To Issuer");
+        cy.get("[data-testid='overlay-title']").should("have.text", "Surrender Document Success");
+      });
+
+      it("should go to verify page, upload a file, connect to wallet and accept the surrendered document successfully", () => {
+        cy.visit("/verify");
+        cy.get("input[type=file]").attachFile("ebl-surrender.json");
+        cy.get("[data-testid='surrenderToIssuer']").should("be.visible");
+        cy.clickConnectAndManageAssetButton();
+        cy.get("[data-testid='acceptSurrenderDropdown']").click();
+        cy.get("[data-testid='acceptSurrenderBtn']").click();
+        cy.confirmMetamaskTransaction();
+        cy.get("[data-testid='overlay-title']").should("have.text", "Surrender Accepted");
       });
     });
   }
