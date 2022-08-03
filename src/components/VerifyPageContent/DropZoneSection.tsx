cd2212b55cacbca44e9182f2f253828c56cc4f6c
isaackps <isaac.kps@gmail.com>
2022-08-03 17:11:37 +0800

    fix :alignment of example image on right side for safari (#598)

--- src/components/VerifyPageContent/DropZoneSection.tsx
+++ src/components/VerifyPageContent/DropZoneSection.tsx
@@ -13,33 +13,40 @@ interface DraggableDemoCertificateProps {
 }
 
 const DraggableDemoCertificate: FunctionComponent<DraggableDemoCertificateProps> = (props) => (
-  <div className="hidden md:block w-full md:w-1/2 px-4">
-    <div className="relative w-full h-full">
-      <img
-        src="/static/images/dropzone/drop_arrow.svg"
-        className="absolute -left-28 lg:-left-24 xl:-left-32 top-1/4"
-        style={{ width: "135px" }}
-      />
-      <div className="flex justify-center items-center w-full h-full">
-        <div className="w-2/3 lg:w-1/2 xl:w-64 mr-auto px-4 inline-block box-content">
-          <div className="aspect-w-1 aspect-h-1">
-            <div className="rounded-full w-full h-full bg-cerulean-500 flex ">
-              <p className="text-base px-5 text-center text-white font-gilroy-bold absolute top-1/2">
-                {roundInstructionsText}
-              </p>
-            </div>
-            <div
-              className="absolute"
-              draggable
-              onDragStart={(e) => e.dataTransfer.setData(getDemoCert(props.chainId), "true")}
-            >
-              <a href={getDemoCert(props.chainId)} download="demo.tt" rel="noindex nofollow" className="cursor-default">
-                <img
-                  draggable="false"
-                  className="top-[-40%] absolute cursor-grab active:cursor-grabbing"
-                  src="/static/images/dropzone/certificate.svg"
-                />
-              </a>
+  <div className="hidden md:block w-full md:w-1/2 px-4 relative">
+    <div className="absolute top-0 left-0 w-full h-full">
+      <div className="relative w-full h-full my-auto">
+        <img
+          src="/static/images/dropzone/drop_arrow.svg"
+          className="absolute -left-28 lg:-left-24 xl:-left-32 top-1/4"
+          style={{ width: "135px" }}
+        />
+        <div className="flex justify-center items-center w-full h-full">
+          <div className="w-2/3 lg:w-1/2 xl:w-64 mr-auto px-4 inline-block box-content">
+            <div className="aspect-w-1 aspect-h-1">
+              <div className="rounded-full w-full h-full bg-cerulean-500 flex ">
+                <p className="text-base px-5 text-center text-white font-gilroy-bold absolute top-1/2">
+                  {roundInstructionsText}
+                </p>
+              </div>
+              <div
+                className="absolute"
+                draggable
+                onDragStart={(e) => e.dataTransfer.setData(getDemoCert(props.chainId), "true")}
+              >
+                <a
+                  href={getDemoCert(props.chainId)}
+                  download="demo.tt"
+                  rel="noindex nofollow"
+                  className="cursor-default"
+                >
+                  <img
+                    draggable="false"
+                    className="top-[-40%] absolute cursor-grab active:cursor-grabbing"
+                    src="/static/images/dropzone/certificate.svg"
+                  />
+                </a>
+              </div>
             </div>
           </div>
         </div>
@@ -54,7 +61,7 @@ export const DropZoneSectionContainer = (): React.ReactElement => {
   const { currentChainId } = useProviderContext();
 
   return currentChainId ? (
-    <div className="flex flex-wrap md:flex-nowrap mt-4 -mx-4">
+    <div className="flex mt-4 -mx-4">
       <div className="w-full md:w-1/2">
         <div
           id="demoDrop"
