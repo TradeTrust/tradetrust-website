a9a5e49c77b632463cfdc4623002144f7cd05d07
Sim Boon Long <simboonlong@gmail.com>
2022-09-14 11:55:40 +0800

    feat: action switch network

---
+++ src/common/hooks/useNetworkSelect.tsx
@@ -0,0 +1,33 @@
+import React, { useCallback, useContext } from "react";
+import { useProviderContext } from "../contexts/provider";
+import { OverlayContext, showDocumentTransferMessage } from "@govtechsg/tradetrust-ui-components";
+import { LoadingModal } from "../../components/UI/Overlay";
+import { ChainId } from "../../constants/chain-info";
+
+interface useNetworkSelectProps {
+  switchNetwork: (chainId: ChainId) => void;
+}
+
+export const useNetworkSelect = (): useNetworkSelectProps => {
+  const { changeNetwork } = useProviderContext();
+  const { showOverlay, closeOverlay } = useContext(OverlayContext);
+
+  const switchNetwork = useCallback(
+    async (chainId: ChainId) => {
+      try {
+        showOverlay(<LoadingModal title={"Changing Network..."} content={"Please respond to the metamask window"} />);
+        await changeNetwork(chainId);
+        closeOverlay();
+      } catch (e: any) {
+        showOverlay(
+          showDocumentTransferMessage("You've cancelled changing network.", {
+            isSuccess: false,
+          })
+        );
+      }
+    },
+    [changeNetwork, closeOverlay, showOverlay]
+  );
+
+  return { switchNetwork };
+};
--- src/components/HomePageContent/HomePageContainer.tsx
+++ src/components/HomePageContent/HomePageContainer.tsx
@@ -1,28 +1,74 @@
 import React from "react";
-import queryString from "query-string";
 import { useLocation } from "react-router-dom";
+import { CONSTANTS } from "@govtechsg/tradetrust-utils";
 import {
-  resetCertificateState,
+  updateCertificate,
   retrieveCertificateByAction,
   retrieveCertificateByActionFailure,
-  updateCertificate,
 } from "../../reducers/certificate";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router";
 import { NestedDocumentState } from "../../constants/NestedDocumentState";
 import { getLogger } from "../../utils/logger";
-
 import { WelcomeSection } from "./WelcomeSection";
 import { MainBenefitsSection } from "./MainBenefitsSection";
 import { HowItWorksSection } from "./HowItWorksSection";
+import { useNetworkSelect } from "../../common/hooks/useNetworkSelect";
+import { ChainId } from "../../constants/chain-info";
 
 const { error } = getLogger("component:mainpage");
+const { TYPES, MESSAGES } = CONSTANTS;
+
+interface Payload {
+  uri: string;
+  permittedActions: string[];
+  redirect: string;
+  key?: string;
+  chainId?: ChainId;
+}
+
+interface Action {
+  type: "DOCUMENT";
+  payload: Payload;
+}
 
 export const HomePageContainer = (): React.ReactElement => {
   const location = useLocation();
   const history = useHistory();
   const dispatch = useDispatch();
+  const { switchNetwork } = useNetworkSelect();
   const loadCertificate = React.useCallback((payload: any) => dispatch(updateCertificate(payload)), [dispatch]);
+
+  React.useEffect(() => {
+    const { search, hash } = location;
+    const params = new URLSearchParams(search);
+    const query = params.get("q");
+
+    const setProviderNetworkToMatch = async (chainId: ChainId, payload: Payload, anchor: string) => {
+      await switchNetwork(chainId);
+      dispatch(retrieveCertificateByAction(payload, anchor));
+    };
+
+    if (query) {
+      const action: Action = JSON.parse(query);
+      const { type, payload } = action;
+      const { chainId } = payload;
+
+      const anchorStr = decodeURIComponent(hash.substr(1));
+      const anchor = anchorStr ? JSON.parse(anchorStr) : {}; // https://github.com/TradeTrust/tradetrust-website/pull/397
+
+      if (type !== "DOCUMENT") {
+        dispatch(retrieveCertificateByActionFailure(`The type ${type} provided from the action is not supported`));
+      } else if (chainId === undefined) {
+        dispatch(retrieveCertificateByActionFailure(MESSAGES[TYPES.NETWORK_INVALID].failureMessage));
+      } else {
+        setProviderNetworkToMatch(chainId, payload, anchor);
+      }
+
+      history.push("/verify");
+    }
+  }, [dispatch, history, location, switchNetwork]);
+
   // event listener for any custom postMessage
   window.addEventListener("message", (event) => {
     if (event.data.type === NestedDocumentState.LOAD) {
@@ -35,23 +81,7 @@ export const HomePageContainer = (): React.ReactElement => {
       }
     }
   });
-  React.useEffect(() => {
-    if (location.search !== "") {
-      const queryParams = queryString.parse(location.search);
-      const anchorStr = decodeURIComponent(location.hash.substr(1));
-      const anchor = anchorStr ? JSON.parse(anchorStr) : {};
-      dispatch(resetCertificateState());
-      const action = JSON.parse(queryParams.q as string);
-      if (action.type === "DOCUMENT") {
-        dispatch(retrieveCertificateByAction(action.payload, anchor));
-      } else {
-        dispatch(
-          retrieveCertificateByActionFailure(`The type ${action.type} provided from the action is not supported`)
-        );
-      }
-      history.push("/verify");
-    }
-  }, [dispatch, location, history]);
+
   return (
     <div className="text-lg">
       <WelcomeSection />
