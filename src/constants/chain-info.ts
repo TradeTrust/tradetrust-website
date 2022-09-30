28838202a6a1a5467b97b83713a842d98fb5044f
Seth <hullogh@gmail.com>
2022-09-30 14:53:43 +0800

    chore: update rinkeby default rpc

--- src/constants/chain-info.ts
+++ src/constants/chain-info.ts
@@ -65,6 +65,7 @@ export const ChainInfo: ChainInfo = {
     networkName: "rinkeby",
     networkLabel: "Rinkeby",
     explorerUrl: "https://rinkeby.etherscan.io",
+    rpcUrl: "https://ethereum-rinkeby-rpc.allthatnode.com",
   },
   [ChainId.Goerli]: {
     label: "Goerli",
