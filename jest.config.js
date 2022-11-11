7e7f5c30e12558e5da3c6e82bba5bc43648ea4fd
Puay Hiang <68947167+puayhiang@users.noreply.github.com>
2022-11-11 17:22:34 +0800

    jest config ignore some transform

--- jest.config.js
+++ jest.config.js
@@ -18,5 +18,5 @@ module.exports = {
   setupFilesAfterEnv: ["<rootDir>/jest.dom.setup.ts"],
   testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)test.[jt]s?(x)"],
   testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/tests/"],
-  transformIgnorePatterns: [],
+  transformIgnorePatterns: ["node_modules/(?!swiper|ssr-window|dom|@govtechsg).*/"],
 };
