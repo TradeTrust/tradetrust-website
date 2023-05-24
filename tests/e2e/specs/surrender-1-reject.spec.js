19d2dc0ee891420f208caab3bfa4e3be834b23dc
isaackps <isaac.kps@gmail.com>
2023-05-24 21:30:08 +0800

    Feat/update e2e test (#758)
    * feat: update synpress and setup synpress to latest
    
    * feat: update synpress and setup synpress to latest
    
    * fix: update setup contracts
    
    * fix: setup contracts code
    
    * fix: test e2e cases
    
    * fix: e2e test cases
    
    * fix: clean up and update fixtures
    
    * fix: update switch account and commented
    
    * fix: constant
    
    * fix: commented

--- tests/e2e/specs/surrender-1-reject.spec.js
+++ tests/e2e/specs/surrender-1-reject.spec.js
@@ -1,6 +1,7 @@
 describe("Surrender-Reject", () => {
   context("Surrender", () => {
     it("should go to verify page, upload a file, connect to wallet and surrender a document successfully", () => {
+      cy.switchMetamaskAccount(1); // need to switch to or make sure it is at account 1 as sometimes it will fail when not in account 1
       cy.visit("/verify");
       cy.get("input[type=file]").attachFile("ebl-surrender.json");
       cy.get("[data-testid='asset-title-owner']").should("be.visible");
@@ -9,8 +10,8 @@ describe("Surrender-Reject", () => {
       cy.get("[data-testid='surrenderDropdown']").click(); // Surrender Document
       cy.get("[data-testid='surrenderBtn']").click();
       cy.confirmMetamaskTransaction();
-      cy.get("#surrender-sign").should("have.text", "Surrendered To Issuer");
       cy.get("[data-testid='overlay-title']").should("have.text", "Surrender Document Success");
+      cy.get("#surrender-sign").should("have.text", "Surrendered To Issuer");
     });
   });
 
