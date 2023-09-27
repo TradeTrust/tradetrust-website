383fc16c39012dafe3d3f7af3bdb83fffe4a86e2
isaackps <isaac.kps@gmail.com>
2023-09-27 14:27:08 +0800

    fix: update text (#829)
    * fix: update text
    
    * fix: text
    
    * fix: update legal article

--- src/components/LegalityPageContent/LegalityPageContent.tsx
+++ src/components/LegalityPageContent/LegalityPageContent.tsx
@@ -93,7 +93,8 @@ const SectionGuide = () => {
         </div>
         <div className="w-full text-center my-8">
           <p className="text-cloud-400">
-            Brought to you jointly by IMDA and Stephenson Harwood LLP, with input on US aspects from Blank Rome LLP
+            Brought to you jointly by IMDA and Stephenson Harwood LLP (in alliance with Virtus Law LLP), with input on
+            US aspects from Blank Rome LLP
           </p>
         </div>
         <div className="flex flex-wrap py-8 justify-center -mx-4">
@@ -139,7 +140,7 @@ const SectionWhatMletr = () => {
         The Model Law on Electronic Transferable Records (MLETR), published by the United Nations Commission on
         International Trade Law (UNCITRAL) in 2017, aims to enable the legal use of electronic transferable records both
         domestically and across borders. 7 jurisdictions, including Singapore have adopted MLETR. United Kingdom’s
-        Electronic Trade Documents Act will come into effect on 20th September 2023.
+        Electronic Trade Documents Act came into effect on 20th September 2023.
       </p>
       <p>
         TradeTrust (TT), a framework and digital utility that enables trusted interoperability of electronic trade
