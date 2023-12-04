5cd0306266fa282084e9fa2e610fa5e182d9b3f3
piruthivi2 <piruthivi@techbreakthrough.co.in>
2023-12-04 15:02:10 +0530

    feat(hedera): added support for hedera network
    added mainnet and testnet hedera network configurations.

--- src/config/chain-config.ts
+++ src/config/chain-config.ts
@@ -7,9 +7,10 @@ export const MAIN_NETWORKS = [
   ChainId.Ethereum, //
   ChainId.Polygon,
   ChainId.XDC,
+  ChainId.HederaMainnet,
 ];
 
 /**
  * Supported networks in development environment
  */
-export const TEST_NETWORKS = [ChainId.Sepolia, ChainId.PolygonMumbai, ChainId.APOTHEM];
+export const TEST_NETWORKS = [ChainId.Sepolia, ChainId.PolygonMumbai, ChainId.APOTHEM, ChainId.HederaTestnet];
