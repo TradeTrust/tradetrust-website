23c04952bcc6c50c698ed8aa4e8779e51b4ac901
rongquan <85145303+rongquan1@users.noreply.github.com>
2024-04-25 11:14:34 +0800

    rendering for idvc fragments

--- src/sagas/certificate.ts
+++ src/sagas/certificate.ts
@@ -8,7 +8,7 @@ import {
 } from "../reducers/certificate";
 import { processQrCode } from "../services/qrProcessor";
 import { verifyDocument } from "../services/verify";
-import { isValid } from "@tradetrust-tt/tt-verify";
+import { isRendered } from "@tradetrust-tt/tt-verify";
 import { decryptString } from "@govtechsg/oa-encryption";
 import { history } from "../history";
 import { CONSTANTS } from "@tradetrust-tt/tradetrust-utils";
@@ -30,9 +30,9 @@ export function* verifyCertificate(): any {
 
     // Instead of success/failure, report completeness
     yield put(verifyingCertificateCompleted(verificationStatus));
-    if (isValid(verificationStatus)) {
-      yield history.push("/viewer");
-    }
+     if (isRendered(verificationStatus)) {
+       yield history.push("/viewer");
+     }
   } catch (e) {
     yield put(verifyingCertificateFailure(TYPES.CLIENT_NETWORK_ERROR));
   }
