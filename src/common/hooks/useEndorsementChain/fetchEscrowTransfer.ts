d58002a172cc1e36c850e79347bf5069468a0cf9
nghaninn <han-inn.ng@sgtradex.com>
2024-10-15 17:44:32 +0800

    chore: resolve test error

--- src/common/hooks/useEndorsementChain/fetchEscrowTransfer.ts
+++ src/common/hooks/useEndorsementChain/fetchEscrowTransfer.ts
@@ -7,22 +7,6 @@ import {
   TokenTransferEventType,
   TransferBaseEvent,
 } from "../../../types";
-import { TitleEscrowInterface } from "../../contexts/TokenInformationContext";
-
-// TODO: HAN Remove V5 Token Registry function
-export const fetchEscrowTransfers = async (
-  provider: providers.Provider,
-  address: string
-): Promise<TitleEscrowTransferEvent[]> => {
-  const titleEscrowContract = TitleEscrow__factory.connect(address, provider);
-  const isTitleEscrow = await titleEscrowContract.supportsInterface(TitleEscrowInterface.V4);
-  if (!isTitleEscrow) throw new Error(`Contract ${address} is not a title escrow contract`);
-  // https://ethereum.stackexchange.com/questions/109326/combine-multiple-event-filters-in-ethersjs
-  const holderChangeLogsDeferred = fetchHolderTransfers(titleEscrowContract, provider);
-  const ownerChangeLogsDeferred = fetchOwnerTransfers(titleEscrowContract, provider);
-  const [holderChangeLogs, ownerChangeLogs] = await Promise.all([holderChangeLogsDeferred, ownerChangeLogsDeferred]);
-  return [...holderChangeLogs, ...ownerChangeLogs];
-};
 
 export const fetchEscrowTransfersV5 = async (
   provider: providers.Provider,
@@ -31,26 +15,6 @@ export const fetchEscrowTransfersV5 = async (
   const titleEscrowContract = TitleEscrow__factory.connect(address, provider);
   const holderChangeLogsDeferred = await fetchAllTransfers(titleEscrowContract, provider);
   return holderChangeLogsDeferred;
-};
-
-/*
-  Retrieve all events that emits BENEFICIARY_TRANSFER 
-*/
-export const fetchOwnerTransfers = async (
-  titleEscrowContract: TitleEscrow,
-  provider: providers.Provider
-): Promise<TitleEscrowTransferEvent[]> => {
-  const ownerChangeFilter = titleEscrowContract.filters.BeneficiaryTransfer(null, null);
-  const ownerChangeLogs = await provider.getLogs({ ...ownerChangeFilter, fromBlock: 0 });
-
-  const ownerChangeLogsParsed = getParsedLogs(ownerChangeLogs, titleEscrowContract);
-  return ownerChangeLogsParsed.map((event) => ({
-    type: "TRANSFER_BENEFICIARY",
-    owner: event.args.toBeneficiary,
-    blockNumber: event.blockNumber,
-    transactionHash: event.transactionHash,
-    transactionIndex: event.transactionIndex,
-  }));
 };
 
 interface ParsedLog {
@@ -77,25 +41,6 @@ export const getParsedLogs = (logs: providers.Log[], titleEscrow: TitleEscrow): 
       ...titleEscrow.interface.parseLog(log),
     };
   });
-};
-
-/*
-  Retrieve all events that emits HOLDER_TRANSFER 
-*/
-export const fetchHolderTransfers = async (
-  titleEscrowContract: TitleEscrow,
-  provider: providers.Provider
-): Promise<TitleEscrowTransferEvent[]> => {
-  const holderChangeFilter = titleEscrowContract.filters.HolderTransfer(null, null);
-  const holderChangeLogs = await provider.getLogs({ ...holderChangeFilter, fromBlock: 0 });
-  const holderChangeLogsParsed = getParsedLogs(holderChangeLogs, titleEscrowContract);
-  return holderChangeLogsParsed.map((event) => ({
-    type: "TRANSFER_HOLDER",
-    blockNumber: event.blockNumber,
-    holder: event.args.toHolder,
-    transactionHash: event.transactionHash,
-    transactionIndex: event.transactionIndex,
-  }));
 };
 
 /*
