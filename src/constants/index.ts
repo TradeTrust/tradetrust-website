a0f0f4dd456da3f7b7be4d4956272aa7533979d6
Ng Han Inn <43451336+nghaninn@users.noreply.github.com>
2024-12-04 15:29:34 +0800

    fix: tr dev url (#961)

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,11 +1,11 @@
-export enum URLS {
-  INFO = "https://tradetrust.io",
-  REF = "https://ref.tradetrust.io",
-  CREATOR = "https://creator.tradetrust.io/",
-  GITHUB = "https://github.com/TradeTrust/tradetrust-website",
-  DOCS = "https://docs.tradetrust.io/",
-  FAQ = "https://www.tradetrust.io/common-error-faqs/",
-}
+export const URLS = {
+  INFO: "https://tradetrust.io",
+  REF: process.env.NODE_ENV === "development" ? "https://dev.tradetrust.io" : "https://ref.tradetrust.io",
+  CREATOR: "https://creator.tradetrust.io/",
+  GITHUB: "https://github.com/TradeTrust/tradetrust-website",
+  DOCS: "https://docs.tradetrust.io/",
+  FAQ: "https://www.tradetrust.io/common-error-faqs/",
+};
 
 export enum FaqType {
   GENERAL = "General",
