383c3f4fce4c30647907b152cb91506be29f6e3d
nghaninn <43451336+nghaninn@users.noreply.github.com>
2024-12-04 15:02:08 +0800

    fix: tr v5 dev url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,12 +1,15 @@
-export enum URLS {
-  INFO = "https://tradetrust.io",
-  REF = "https://ref.tradetrust.io",
-  REF_V5TR = "https://v5-token-registry.tradetrust.io",
-  CREATOR = "https://creator.tradetrust.io/",
-  GITHUB = "https://github.com/TradeTrust/tradetrust-website",
-  DOCS = "https://docs.tradetrust.io/",
-  FAQ = "https://www.tradetrust.io/common-error-faqs/",
-}
+export const URLS = {
+  INFO: "https://tradetrust.io",
+  REF: "https://ref.tradetrust.io",
+  REF_V5TR:
+    process?.env?.NODE_ENV === "development"
+      ? "https://v5-token-registry.dev.tradetrust.io"
+      : "https://v5-token-registry.tradetrust.io",
+  CREATOR: "https://creator.tradetrust.io/",
+  GITHUB: "https://github.com/TradeTrust/tradetrust-website",
+  DOCS: "https://docs.tradetrust.io/",
+  FAQ: "https://www.tradetrust.io/common-error-faqs/",
+};
 
 export enum FaqType {
   GENERAL = "General",
