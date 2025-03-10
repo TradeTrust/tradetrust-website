3f2f6378018597dcc20ebc7a83d718f5d7ca9187
Rishabh Singh <rishabh7singh5@gmail.com>
2025-03-10 07:49:41 +0530

    fix: check for metamask

--- tests/e2e/specs/1a-transfer-holder.spec.js
+++ tests/e2e/specs/1a-transfer-holder.spec.js
@@ -4,25 +4,23 @@ before(() => {
   cy.window().then((window) => {
     window.localStorage.setItem("hasSeenPopup", "true");
   });
-  cy.wait(10000);
+  cy.wait(1000);
   cy.createMetamaskAccount();
-  cy.wait(10000);
+  cy.wait(1000);
   cy.createMetamaskAccount();
-  // cy.wait(10000);
+  cy.wait(1000);
   // cy.importMetamaskAccount("0xc58c1ff75001afdca8cecb61b47f36964febe4188b8f7b26252286ecae5a8879");
-  cy.wait(10000);
   cy.switchMetamaskAccount(1); // ensure switch to account 1 (owner)
-  cy.ensureMetamaskIsInstalled();
 });
 
 describe("Transfer Holder", () => {
   it("should go to verify page, upload a file, connect to wallet and transfer holder successfully", () => {
     cy.visit("/");
-    cy.connectToMetamaskWalletAndApproveAllAccounts();
     cy.waitAndUploadFile("ebl-transfer-holder.json");
+    cy.get("[data-testid='overlayHandleDispatchBtn']").click();
     cy.get("[data-testid='asset-title-owner']").should("be.visible");
     cy.get("[data-testid='asset-title-holder']").should("be.visible");
-
+    cy.connectToMetamaskWalletAndApproveAllAccounts();
     cy.get("[data-testid='manageAssetDropdown']").click();
     cy.get("[data-testid='transferHolderDropdown']").click(); // Transfer Holdership
     cy.get("[data-testid='editable-input-holder']").type(ACCOUNT_2);
