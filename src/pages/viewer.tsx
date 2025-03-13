85816203d27b6f920efb5628abe52efa3131a684
rongquan1 <rongquan.low@gmail.com>
2025-03-13 10:18:30 +0800

    feat: update ui

--- src/pages/viewer.tsx
+++ src/pages/viewer.tsx
@@ -1,15 +1,16 @@
-import { OverlayContent, OverlayContextProvider } from "@tradetrust-tt/tradetrust-ui-components";
 import React from "react";
 import { Helmet } from "react-helmet";
-import ConnectToMetamask from "../components/ConnectToMetamask";
-import { NetworkSelect } from "../components/Layout/NetworkSelect";
-import { InfoOverlay } from "../components/UI/Overlay";
 import { ViewerPageContainer } from "../components/ViewerPageContainer";
+import { NetworkSection } from "../components/NetworkSection";
+import { useSelector } from "react-redux";
+import { RootState } from "../reducers";
 
 interface ViewerPageInterface {
   isMagicDemo?: boolean;
 }
 export const ViewerPage = (props: ViewerPageInterface): React.ReactElement => {
+  const rootState = useSelector((state: RootState) => state);
+  const document = rootState.certificate.rawModified;
   return (
     <>
       <Helmet>
@@ -34,25 +35,8 @@ export const ViewerPage = (props: ViewerPageInterface): React.ReactElement => {
         <div className="container">
           <h2 className="text-cloud-800 max-w-3xl">Verify Documents</h2>
         </div>
-        <div className="container flex flex-col xs:flex-row flex-wrap md:flex-nowrap items-start md:items-center justify-between gap-2">
-          <div className="flex flex-wrap md:flex-nowrap gap-2 w-full md:w-auto">
-            <div className="w-full xs:w-auto text-gray-900 flex items-center" data-testid="page-subtitle">
-              Document verified on
-            </div>
-            <div className="w-full xs:w-auto flex flex-row items-center">
-              <NetworkSelect />
-              <OverlayContextProvider>
-                <InfoOverlay className="p-0 ml-3 cursor-pointer focus:outline-none">
-                  <OverlayContent className="bg-white max-w-sm lg:max-w-md" title="Network Selector">
-                    A document can only be successfully verified on the same network where the document was created in.
-                    <br />
-                    If unsure, do check with the document issuer.
-                  </OverlayContent>
-                </InfoOverlay>
-              </OverlayContextProvider>
-            </div>
-          </div>
-          <ConnectToMetamask className="w-full xs:w-[18.25rem]" />
+        <div className="container">
+          <NetworkSection subtitle="Document verified on" overlayMargin="ml-3" disabled={true} document={document} />
         </div>
         <ViewerPageContainer {...props} />
       </div>
