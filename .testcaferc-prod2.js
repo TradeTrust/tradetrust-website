3a66c1d5280ec4e42257513c999aa7f0088fd9a8
rongquan1 <rongquan.low@gmail.com>
2025-03-13 15:36:01 +0800

    fix: update testcafe settings

--- .testcaferc-prod2.js
+++ .testcaferc-prod2.js
@@ -7,7 +7,7 @@ module.exports = {
     "src/**/nested-documents.spec.ts"
   ],
   "browsers": [
-    "remote chrome:headless --no-native --disable-web-security --user-data-dir=/tmp/chrome --autoplay-policy=no-user-gesture-required --allow-insecure-localhost --ignore-certificate-errors"
+    "chrome:headless --disable-web-security --autoplay-policy=no-user-gesture-required --allow-insecure-localhost --ignore-certificate-errors"
   ],
   "concurrency": 4,
   "ssl": "pfx=src/test/ca/testingdomain.pfx;rejectUnauthorized=true",
