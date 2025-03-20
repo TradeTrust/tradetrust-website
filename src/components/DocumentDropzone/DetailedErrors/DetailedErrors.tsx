699952a07dd0790870b9a1ca03b8f65dc4f4741d
rongquan1 <rongquan.low@gmail.com>
2025-03-20 09:47:50 +0800

    fix: error handling message

--- src/components/DocumentDropzone/DetailedErrors/DetailedErrors.tsx
+++ src/components/DocumentDropzone/DetailedErrors/DetailedErrors.tsx
@@ -5,7 +5,10 @@ import { errorMessages, errorMessageHandling } from "@trustvc/trustvc";
 export const DetailedError: FunctionComponent<{ title: string; message: string }> = ({ title, message }) => {
   return (
     <div className="my-2 sm:mx-8 xl:mx-16">
-      <h4 className="text-scarlet-500 mb-0">{title}</h4>
+      <div className="flex items-center justify-center gap-2">
+        <img src="/static/images/dropzone/invalid.svg" alt="Document invalid" className="w-6 h-6" />
+        <h4 className="text-scarlet-500 mb-0">{title}</h4>
+      </div>
       <p className="text-cloud-800 break-words">{message}</p>
     </div>
   );
@@ -15,10 +18,22 @@ export const DetailedErrors: FunctionComponent<{
   verificationStatus: VerificationFragment[] | null;
   verificationError: string | null;
 }> = ({ verificationStatus, verificationError }) => {
-  if (!verificationStatus) return null;
   const { MESSAGES } = errorMessages;
-  const errors = errorMessageHandling(verificationStatus);
-  if (verificationError) errors.push(verificationError);
+  const errors: string[] = [...(verificationStatus ? errorMessageHandling(verificationStatus) : [])];
+  if (verificationError) {
+    if (Array.isArray(verificationError)) {
+      verificationError.forEach((error) => {
+        if (MESSAGES[error] && !errors.includes(error)) {
+          errors.push(error);
+        }
+      });
+    } else {
+      if (MESSAGES[verificationError] && !errors.includes(verificationError)) {
+        errors.push(verificationError);
+      }
+    }
+  }
+
   return (
     <div className="mb-8">
       {errors.map((errorType, index) => (
