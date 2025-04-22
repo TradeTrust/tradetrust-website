6423b942965f2d95c6d905f4af03832377af906e
Rishabh Singh <rishabh7singh5@gmail.com>
2025-04-22 07:58:53 +0530

    feat: document preview modal

---
+++ src/components/Landing/FormSelect/ExpandPreview.tsx
@@ -0,0 +1,103 @@
+import { Button } from "@tradetrust-tt/tradetrust-ui-components";
+import React, { FunctionComponent, useEffect, useState } from "react";
+
+interface ConnectMetamaskOverlayProps {
+  handleConnection: () => void;
+  form: any;
+}
+
+export const RejectActionTitle = {
+  HOLDERSHIP: "Holdership",
+  OWNERSHIP: "Ownership",
+  OWNERSHIP_AND_HOLDERSHIP: "Ownership & Holdership",
+};
+
+export const ExpandPreview: FunctionComponent<ConnectMetamaskOverlayProps> = ({ handleConnection, form }) => {
+  const [zoom, setZoom] = useState(50);
+
+  useEffect(() => {
+    const range = document.querySelector(".range-custom") as HTMLInputElement;
+    if (range) {
+      range.style.setProperty("--val", `${zoom}%`);
+    }
+  }, [zoom]);
+
+  return (
+    <div
+      data-testid="connect-metamask-overlay"
+      id="overlay"
+      className="font-gilroy bg-white w-full max-w-[640px] min-w-[308px] h-auto flex flex-col rounded-[12px] font-medium leading-5 tracking-[0px] text-neutral-600 z-20"
+    >
+      <div id="header" className="flex flex-none items-center gap-4 min-[596px]:flex-nowrap p-6 pb-4">
+        <h3>Document Preview</h3>
+      </div>
+
+      <div className="flex items-center flex-row px-6 w-full">
+        <div className="w-1/2">
+          <p>{form.name}</p>
+        </div>
+
+        <div className="w-1/2 flex items-center justify-between">
+          <div className="flex items-center justify-between w-full gap-1 px-2">
+            <p className="cursor-pointer hover:font-bold mb-1" onClick={() => setZoom(Math.max(5, zoom - 5))}>
+              -
+            </p>
+
+            <input
+              type="range"
+              min="5"
+              max="100"
+              value={zoom}
+              onChange={(e) => setZoom(parseInt(e.target.value))}
+              className="h-[3px] w-11/12 range-custom cursor-pointer"
+            />
+
+            <p className="cursor-pointer hover:font-bold mb-1" onClick={() => setZoom(Math.min(100, zoom + 5))}>
+              +
+            </p>
+          </div>
+          <p className="text-sm px-2">{zoom}%</p>
+        </div>
+      </div>
+
+      <div id="body" className="px-6 py-4">
+        <div
+          id="scrollbar"
+          className="w-[592px] mx-auto p-2 h-[444px] overflow-auto bg-cloud-100 rounded-[12px] border border-transparent"
+        >
+          {/* <div id="img-container" className="p-2"> */}
+          <div
+            className="rounded-[12px] "
+            style={{
+              width: `${zoom * 2}%`,
+              height: `${zoom * 2}%`,
+              transformOrigin: "top left",
+            }}
+          >
+            <img src={form.img} alt="Expand" className="rounded-[12px]" />
+            {/* </div> */}
+          </div>
+        </div>
+      </div>
+
+      <div id="footer" className="p-6 pt-4">
+        <div className="flex flex-col xs:flex-row items-center justify-between gap-2 text-center text-xl font-bold leading-6 min-[596px]:flex-nowrap">
+          <Button
+            className="bg-white text-cerulean-500 hover:bg-cloud-100 px-[18px] py-3 w-full xs:w-auto flex-1 min-h-12 rounded-xl p-3"
+            onClick={() => handleConnection()}
+            data-testid={`overlayHandleConnectionBtn`}
+          >
+            Dismiss
+          </Button>
+          <Button
+            className="bg-cerulean-500 text-white hover:bg-cerulean-800 px-[18px] py-3 w-full xs:w-auto flex-1 min-h-12 rounded-xl p-3"
+            onClick={() => console.log("this")}
+            data-testid={`overlayHandleDispatchBtn`}
+          >
+            Create Document
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
