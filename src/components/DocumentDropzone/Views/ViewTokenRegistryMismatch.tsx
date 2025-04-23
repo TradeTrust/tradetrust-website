65e3a44d3c7dadf7eb5188535f2f68309bc621a4
Rishabh Singh <rishabh7singh5@gmail.com>
2025-04-23 13:15:39 +0530

    fix: view token registory mismatch design

--- src/components/DocumentDropzone/Views/ViewTokenRegistryMismatch.tsx
+++ src/components/DocumentDropzone/Views/ViewTokenRegistryMismatch.tsx
@@ -1,8 +1,6 @@
 import React, { FunctionComponent } from "react";
 import { Button, ButtonSize } from "@tradetrust-tt/tradetrust-ui-components";
 import { URLS } from "../../../constants";
-import { TooltipIcon } from "../../UI/SvgIcon";
-import { Info } from "react-feather";
 
 export const ViewTokenRegistryMismatch: FunctionComponent = () => {
   return (
@@ -13,15 +11,19 @@ export const ViewTokenRegistryMismatch: FunctionComponent = () => {
         src="/static/images/dropzone/dropzone_illustration.svg"
       />
       <h4 className="text-scarlet-500">Document cannot be read.</h4>
-      <h4 className="text-scarlet-500 mb-4">Please check that you have a valid .tt or .json file</h4>
-      <h4>Drop your TradeTrust Document to view its contents</h4>
+      <h4 className="text-scarlet-500 ">Please check that you have a valid .tt or .json file</h4>
+      <span className="ml-1">
+        Please visit <span className="text-blue-500">{URLS.REF}</span> to verify older version of transferable
+        documents.
+      </span>
+      <h4 className="mt-4">Drop your TradeTrust Document to view its contents</h4>
       <p className="my-6">Or</p>
       <Button className="bg-cerulean-500 text-white hover:bg-cerulean-800 mr-2 md:mr-0" size={ButtonSize.SM}>
         Select Document
       </Button>
-      <hr className="border-t-2 border-gray-300 my-4" />
+      {/* <hr className="border-t-2 border-gray-300 my-4" /> */}
 
-      <div>
+      {/* <div>
         <TooltipIcon content="Please visit https://ref.tradetrust.io to verify older version of transferable documents.">
           <Info className="text-orange-500" />
         </TooltipIcon>
@@ -30,7 +32,7 @@ export const ViewTokenRegistryMismatch: FunctionComponent = () => {
           Please visit <span className="text-blue-500">{URLS.REF}</span> to verify older version of transferable
           documents.
         </span>
-      </div>
+      </div> */}
     </div>
   );
 };
