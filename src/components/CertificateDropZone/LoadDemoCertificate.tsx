d43d026853d56b7bcc61c6cc8778ea42c1dc845f
Rishabh Singh <rishabh7singh5@gmail.com>
2025-05-06 19:25:57 +0530

    fix: keep same tab

--- src/components/CertificateDropZone/LoadDemoCertificate.tsx
+++ src/components/CertificateDropZone/LoadDemoCertificate.tsx
@@ -31,7 +31,7 @@ export const LoadDemoCertificate: FunctionComponent<LoadDemoCertificateProps> = 
         >
           Load Demo Tradetrust Document
         </Button>
-        <a className="w-full xs:w-72" href={"/creator"} target="_blank" rel="noopener noreferrer">
+        <a className="w-full xs:w-72" href={"/creator"}>
           <Button className="bg-white rounded-xl border-cloud-100 text-cerulean-500 shadow-none hover:bg-cloud-200 w-full">
             Create Tradetrust Document
           </Button>
