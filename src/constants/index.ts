cf19d735896c9110f5ec415b1733be004161aa96
nghaninn <43451336+nghaninn@users.noreply.github.com>
2025-05-07 18:30:23 +0800

    fix: add CREATOR link

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,7 @@
 export const URLS = {
   INFO: "https://tradetrust.io",
   REF: process.env.NODE_ENV === "development" ? "https://dev.tradetrust.io" : "https://ref.tradetrust.io",
+  CREATOR: "https://creator.tradetrust.io/",
   GITHUB: "https://github.com/TradeTrust/tradetrust-website",
   DOCS: "https://docs.tradetrust.io",
   FAQ: "https://www.tradetrust.io/common-error-faqs/",
