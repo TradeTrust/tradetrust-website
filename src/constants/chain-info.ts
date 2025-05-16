ae47268b8aca84ce3a705b50694ae83bcd66ff1f
RishabhS7 <59636880+RishabhS7@users.noreply.github.com>
2025-05-16 09:12:28 +0530

    fix: update networl label (#1022)

--- src/constants/chain-info.ts
+++ src/constants/chain-info.ts
@@ -150,7 +150,7 @@ export const ChainInfo: ChainInfo = {
     },
   },
   [ChainId.StabilityTestnet]: {
-    label: "Stability Testnet",
+    label: "Stability Testnet (Beta)",
     chainId: ChainId.StabilityTestnet,
     iconImage: "/static/images/networks/stability.png",
     networkName: "stabilitytestnet",
@@ -164,7 +164,7 @@ export const ChainInfo: ChainInfo = {
     },
   },
   [ChainId.AstronTestnet]: {
-    label: "Astron Testnet",
+    label: "Astron Testnet (Beta)",
     chainId: ChainId.AstronTestnet,
     iconImage: "/static/images/networks/astron.png",
     networkName: "astrontestnet",
