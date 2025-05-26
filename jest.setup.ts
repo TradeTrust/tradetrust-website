a21327360a7aa71982f5d453f51f35506c1f38bb
nghaninn <43451336+nghaninn@users.noreply.github.com>
2025-05-27 01:22:13 +0800

    chore: reset jest config

--- jest.setup.ts
+++ jest.setup.ts
@@ -10,3 +10,17 @@ Object.assign(global, {
 if (process.env.DEBUG) {
   debug.log = console.log.bind(console);
 }
+
+// polyfill (https://stackoverflow.com/questions/42213522/mocking-document-createrange-for-jest)
+const createRange = () => ({
+  setStart: () => {},
+  setEnd: () => {},
+  commonAncestorContainer: {
+    nodeName: "BODY",
+    ownerDocument: document,
+  },
+});
+window.alert = jest.fn();
+window.fetch = jest.fn();
+
+Object.defineProperty(document, "createRange", createRange);
