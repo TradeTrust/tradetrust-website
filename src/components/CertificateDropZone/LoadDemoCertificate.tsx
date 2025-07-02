13688e2cca02b08cf8c2b50354d39db070bcf495
rongquan1 <85145303+rongquan1@users.noreply.github.com>
2025-07-02 17:50:53 +0800

    fix: bug fix on unsupported chain id (#1049)
    * fix: bug fix on unsupported chain id
    
    * fix: bug fix

--- src/components/CertificateDropZone/LoadDemoCertificate.tsx
+++ src/components/CertificateDropZone/LoadDemoCertificate.tsx
@@ -3,15 +3,19 @@ import { useDispatch } from "react-redux";
 import { updateCertificate } from "../../reducers/certificate";
 import { Button } from "../Button";
 import { loadDemoCertificate } from "../VerifyPageContent/helpers";
-import { reset, setActive } from "../../reducers/sample";
+import { setActive } from "../../reducers/sample";
 import { ChainId } from "../../constants/chain-info";
 import { Link } from "react-router-dom";
+import { getChainInfoFromNetworkName } from "../../common/utils/chain-utils";
+import { NETWORK_NAME } from "../../config";
+import { useNetworkSelect } from "../../common/hooks/useNetworkSelect";
 interface LoadDemoCertificateProps {
   currentChainId: ChainId | undefined;
 }
 export const LoadDemoCertificate: FunctionComponent<LoadDemoCertificateProps> = ({ currentChainId }) => {
   const dispatch = useDispatch();
   const loadCertificate = React.useCallback((payload: any) => dispatch(updateCertificate(payload)), [dispatch]);
+  const { switchNetwork } = useNetworkSelect();
 
   return (
     <div className="text-center">
@@ -21,13 +25,10 @@ export const LoadDemoCertificate: FunctionComponent<LoadDemoCertificateProps> = 
       <div className="flex flex-col xs:flex-row justify-center gap-2 mt-4">
         <Button
           className="bg-white rounded-xl border-cloud-100 text-cerulean-500 shadow-none hover:bg-cloud-200 w-full xs:w-72"
-          onClick={() => {
-            if (currentChainId) {
-              loadDemoCertificate(loadCertificate, currentChainId);
-              dispatch(setActive());
-            } else {
-              dispatch(reset());
-            }
+          onClick={async () => {
+            const chainIdToUse = currentChainId ?? getChainInfoFromNetworkName(NETWORK_NAME).chainId;
+            await loadDemoCertificate(loadCertificate, chainIdToUse, switchNetwork);
+            dispatch(setActive());
           }}
         >
           Load Demo Tradetrust Document
