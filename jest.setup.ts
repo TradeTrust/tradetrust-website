d8fb6b80744a0f81f96596c6f6dd3dfcb788c13e
Rishabh Singh <rishabh7singh5@gmail.com>
2025-08-21 13:42:07 +0530

    fix: esm jest issue

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,8 +1,13 @@
 import debug from "debug";
 import { TextEncoder, TextDecoder } from "util";
+import { Crypto, CryptoKey } from "@peculiar/webcrypto";
+
+const crypto = new Crypto();
 Object.assign(global, {
   TextDecoder,
   TextEncoder,
+  crypto,
+  CryptoKey,
 });
 // jest.setup.js
 
@@ -22,5 +27,10 @@ const createRange = () => ({
 });
 window.alert = jest.fn();
 window.fetch = jest.fn();
+
+// // Polyfill Web Crypto
+// const cryptoInstance = new Crypto();
+// (globalThis as any).crypto = cryptoInstance;
+// (global as any).crypto.subtle = cryptoInstance.subtle;
 
 Object.defineProperty(document, "createRange", createRange);
