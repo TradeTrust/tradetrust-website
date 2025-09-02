6e167f72157f8270caf5bcfe34adba022d885508
Rishabh Singh <rishabh7singh5@gmail.com>
2025-09-02 15:33:01 +0530

    chore: update test cases for dm v2.0

--- src/components/ObfuscatedMessage/ObfuscatedMessage.tsx
+++ src/components/ObfuscatedMessage/ObfuscatedMessage.tsx
@@ -1,18 +1,26 @@
 import { isObfuscated } from "@trustvc/trustvc";
 import React, { FunctionComponent } from "react";
 import { WrappedOrSignedOpenAttestationDocument } from "../../utils/shared";
+import { useSelector } from "react-redux";
+import { RootState } from "../../reducers";
+import { DOCUMENT_SCHEMA } from "../../reducers/certificate";
 
 interface ObfuscatedMessageProps {
   document: WrappedOrSignedOpenAttestationDocument;
 }
 
 export const ObfuscatedMessage: FunctionComponent<ObfuscatedMessageProps> = ({ document }) => {
-  if (!isObfuscated(document)) return null;
+  const { documentSchema } = useSelector((state: RootState) => state.certificate);
 
+  if (!isObfuscated(document)) return null;
   return (
     <div className="container">
       <div className="text-lg font-gilroy-bold text-scarlet-500" data-testid="obfuscation-info">
-        <p className="py-6">Note: There are fields/data obfuscated in this document.</p>
+        <p className="py-6">
+          {documentSchema === DOCUMENT_SCHEMA.W3C_VC_2_0
+            ? "Note: There are fields/data might be obfuscated in this document."
+            : "Note: There are fields/data obfuscated in this document."}
+        </p>
       </div>
     </div>
   );
