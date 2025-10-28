aa1ad0845ce9b947277d72c3893d8e0a9d5cc56f
RishabhS7 <59636880+RishabhS7@users.noreply.github.com>
2025-10-28 12:05:23 +0530

    fix: add get headers (#1110)
    * fix: update getHeaders
    
    * fix: update magic link happy flow

--- src/common/API/storageAPI.tsx
+++ src/common/API/storageAPI.tsx
@@ -11,6 +11,7 @@ const fetchCsrfToken = async (documentStorageURL: string): Promise<string> => {
       method: "get",
       url: url,
       withCredentials: true,
+      headers: getHeaders(),
     });
 
     const csrfToken = response.data.csrfToken;
@@ -49,7 +50,7 @@ export const getQueueNumber = async (documentStorageURL: string): Promise<AxiosR
   return axios({
     method: "get",
     url: url,
-    headers: getHeaders(documentStorageURL),
+    headers: getHeaders(),
   });
 };
 
