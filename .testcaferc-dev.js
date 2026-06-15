cf0c8709fbd8e079582afad240188220fb0c03d9
manishdex25 <manish.singh@dextech.ai>
2026-06-16 00:24:29 +0530

    chore: update TestCafe configuration and add new tests for POL minted documents

--- .testcaferc-dev.js
+++ .testcaferc-dev.js
@@ -4,7 +4,8 @@ module.exports = {
   "appCommand": "npm run serve-static",
   "src": [
     "src/**/*.spec.ts",
-    "!src/**/nested-documents.spec.ts"
+    "!src/**/nested-documents.spec.ts",
+    "!src/**/pol-minted.spec.ts"
   ],
   "browsers": [
     "chrome --autoplay-policy=no-user-gesture-required  --allow-insecure-localhost --ignore-certificate-errors --disable-features=LocalNetworkAccessChecks"
